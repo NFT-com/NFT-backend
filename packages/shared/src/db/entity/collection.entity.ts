57b0dc68d8750a0b68ef890bc5c46d99442d6af4
just-toby <ejd4@protonmail.com>
2021-12-16 14:12:15 -0800

    feat: collection entity and db migration

--- packages/shared/src/db/entity/collection.entity.ts
+++ packages/shared/src/db/entity/collection.entity.ts
@@ -4,12 +4,15 @@ import { defs } from '@nftcom/shared'
 
 import { BaseEntity } from './base.entity'
 
-// TODO recheck indexes after some data is available
 @Entity()
 @Index(['userId'])
 export class Collection extends BaseEntity {
 
-  @Column({ nullable: false })
+  @Column('json', {
+    nullable: false,
+    array: true,
+    default: [],
+  })
   items: defs.CollectionItem[]
 
   @Column({ nullable: false })
