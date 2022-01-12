814682dd700ec78d856dd39739a83a25e6bad580
Gavin <gmaijoe@users.noreply.github.com>
2022-01-12 09:59:18 -0400

    Added changes for style / perf (#43)
    * fix: removed indexer code
    
    * feat: updates to allow gk bids
    
    * feat: optional email signup
    
    * feat: working be
    
    * fix: removed unused userError
    
    * Merging Optional Email into Eddie's work (#36)
    
    * fix: removed indexer code
    
    * feat: optional email signup
    
    * feat: working be
    
    * fix: removed unused userError
    
    * fix: migration for GK nft type
    
    * feat: updates to allow gk bids
    
    * fix: migration for GK nft type
    
    * Merging Optional Email into Eddie's work (#36)
    
    * fix: removed indexer code
    
    * feat: optional email signup
    
    * feat: working be
    
    * fix: removed unused userError
    
    * feat: local-merge
    
    * feat: added bull listener for minted profile event
    
    * fix: add internal filter
    
    * fix: working bull
    
    * fix: updated contract address
    
    * fix: merged latest
    
    * feat: initial set of balance checkers and bid validators for profiles
    
    * fix: remove unnecessary delete of losing bids
    
    * fix: some fixes
    
    * fix: working bid validation with balances
    
    * fix: remove unnecessary web3
    
    * feat: update and redeploy
    
    * fix: some PR additions
    
    * fix: added changes
    
    * fix: remove logs
    
    * fix: fix typeorm
    
    * fix: re-add salman
    
    Co-authored-by: just-toby <ejd4@protonmail.com>

--- packages/gql/src/job/handler.ts
+++ packages/gql/src/job/handler.ts
@@ -28,36 +28,71 @@ const filterLiveBids = (bids: entity.Bid[]): Promise<entity.Bid[]> => {
   return Promise.all([
     bids,
     repositories.profile.find({ where: { status: defs.ProfileStatus.Available } })
-      .then(profiles => profiles.map((profile: entity.Profile) => profile.id)),
+      .then(profiles => profiles.map((profile: entity.Profile) => profile.id)
+        .reduce(function(map, item) {
+          map[item] = true
+          return map
+        }, {})), // returns mapping for constant lookup
   ]).then(([bids, availableProfilesIds]: [entity.Bid[], string[]]) => {
     return bids.filter(  // second promise filters all remaining valid bids
       (bid: entity.Bid) =>
-        (bid.nftType == defs.NFTType.Profile &&
-        availableProfilesIds.indexOf(bid.profileId) >= 0 &&
-        bid.status != defs.BidStatus.Executed) ||
-        (bid.nftType == defs.NFTType.GenesisKey &&
-        bid.status != defs.BidStatus.Executed),
+        (bid.nftType == defs.NFTType.Profile && availableProfilesIds[bid.profileId]) ||
+        bid.nftType == defs.NFTType.GenesisKey,
     ) // filter only non-executed bids for available handles and genesis keys
   })
 }
 
 // size of each array for balances for NFT and WETH
+// 450 is chosen as 450 x 2 (balance query for NFT and WETH) = 900
+// 900 balanceOf queries maxes sits comfortably below the 1000 maximum size limit on Ethereum
+// the size limit is mostly due to gas limits per ethereum call.
+// 
+// Even though balanceOf is a view function, it has to conform to size limits on calls via the
+// ethereum nodes
 const perChunk = 450
+
+const getAddressBalanceMapping = (bids: entity.Bid[], walletIdAddressMapping: any, chainId: number):
+[entity.Bid[], entity.Bid[], any, any] => {
+  const splitAddressArrays: any = Object.values(walletIdAddressMapping)
+    .reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index/perChunk)
+  
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = [] // start a new chunk
+      }
+  
+      resultArray[chunkIndex].push(item)
+  
+      return resultArray
+    }, [])
+
+  const profileBids = bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.Profile)
+  const genesisKeyBids = bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.GenesisKey)
+  const addressBalanceMapping = splitAddressArrays.map(
+    splitArray => getAddressesBalances( // returns balances in object, need Object.assign to combine into one single object
+      provider.provider(Number(chainId)),
+      splitArray,
+      [contracts.nftTokenAddress(chainId), contracts.wethAddress(chainId)],
+      contracts.multiBalance(chainId),
+    ),
+  )
+
+  return [profileBids, genesisKeyBids, walletIdAddressMapping, addressBalanceMapping]
+}
 
 // validates live balances for all the filtered bids
 const validateLiveBalances = (bids: entity.Bid[], chainId: number): Promise<boolean> => {
   try {
     return Promise.all([
-      bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.Profile),
-      bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.GenesisKey),
+      bids,
       bids
         .map(bid => bid.walletId)
-        .filter(onlyUnique).map((bidId: string) => { return { id: bidId }}),
+        .filter(onlyUnique).map((walletId: string) => { return { id: walletId }}),
     ]).then(
-      ([profileBids, genesisKeyBids, uniqueWalletIds]: [entity.Bid[], entity.Bid[], any[]]) => {
+      ([bids, uniqueWalletIds]:
+      [entity.Bid[], Array<{ id: string }>]) => {
         return Promise.all([
-          profileBids,
-          genesisKeyBids,
+          bids,
           repositories.wallet.find({ where: uniqueWalletIds }).then((wallets: entity.Wallet[]) =>  {
             // create mapping of walletId => address
             return wallets.reduce((map, walletEntity) =>
@@ -66,76 +101,38 @@ const validateLiveBalances = (bids: entity.Bid[], chainId: number): Promise<bool
         ])
       }).then(
       ([
-        profileBids,
-        genesisKeyBids,
+        bids,
         walletIdAddressMapping,
-      ]: [entity.Bid[], entity.Bid[], any]) => {
-        return Promise.all([
-          Promise.resolve(profileBids),
-          Promise.resolve(genesisKeyBids),
-          Promise.resolve(walletIdAddressMapping),
-          Object.values(walletIdAddressMapping).reduce((resultArray, item, index) => {
-            const chunkIndex = Math.floor(index/perChunk)
-          
-            if (!resultArray[chunkIndex]) {
-              resultArray[chunkIndex] = [] // start a new chunk
-            }
-          
-            resultArray[chunkIndex].push(item)
-          
-            return resultArray
-          }, []),
-        ]).then(
-          ([
-            profileBids,
-            genesisKeyBids,
-            walletIdAddressMapping,
-            splitAddressArrays,
-          ]: [entity.Bid[], entity.Bid[], any, any]) => {
-            return Promise.all([
-              Promise.resolve(profileBids),
-              Promise.resolve(genesisKeyBids),
-              Promise.resolve(walletIdAddressMapping),
-              Promise.all(splitAddressArrays.map(
-                splitArray => getAddressesBalances( // returns balances in object, need Object.assign to combine into one single object
-                  provider.provider(Number(chainId)),
-                  splitArray,
-                  [contracts.nftTokenAddress(chainId), contracts.wethAddress(chainId)],
-                  contracts.multiBalance(chainId),
-                ),
-              )),
-            ])
-          },
-        ).then(
-          ([
-            profileBids,
-            genesisKeyBids,
-            walletIdAddressMapping,
-            addressBalanceMapping,
-          ]: [entity.Bid[], entity.Bid[], any, any]) => {
-            return Promise.all([
-              profileBids.map((bid: entity.Bid) => {
-                const balanceObj =  addressBalanceMapping[0][walletIdAddressMapping[bid.walletId]]
-                const nftBalance = Number(balanceObj[contracts.nftTokenAddress(chainId)]) ?? 0
+      ]: [entity.Bid[], any]) => getAddressBalanceMapping(bids, walletIdAddressMapping, chainId))
+      .then(
+        ([
+          profileBids,
+          genesisKeyBids,
+          walletIdAddressMapping,
+          addressBalanceMapping,
+        ]: [entity.Bid[], entity.Bid[], any, any]) => {
+          return Promise.all([
+            profileBids.map((bid: entity.Bid) => {
+              const balanceObj =  addressBalanceMapping[0][walletIdAddressMapping[bid.walletId]]
+              const nftBalance = Number(balanceObj[contracts.nftTokenAddress(chainId)]) ?? 0
                 
-                if (nftBalance < Number(bid.price)) {
-                  logger.debug('softDeleteProfileBid', { type: bid.nftType, bidAmount: Number(bid.price), nftBalance })
-                  repositories.bid.deleteById(bid.id)
-                }
-              }),
-              genesisKeyBids.map((bid: entity.Bid) => {
-                const balanceObj =  addressBalanceMapping[0][walletIdAddressMapping[bid.walletId]]
-                const wethBalance = Number(balanceObj[contracts.wethAddress(chainId)]) ?? 0
+              if (nftBalance < Number(bid.price)) {
+                logger.debug('softDeleteProfileBid', { type: bid.nftType, bidAmount: Number(bid.price), nftBalance })
+                repositories.bid.deleteById(bid.id)
+              }
+            }),
+            genesisKeyBids.map((bid: entity.Bid) => {
+              const balanceObj =  addressBalanceMapping[0][walletIdAddressMapping[bid.walletId]]
+              const wethBalance = Number(balanceObj[contracts.wethAddress(chainId)]) ?? 0
                 
-                if (wethBalance < Number(bid.price)) {
-                  logger.debug('softDeleteGenesisBid', { type: bid.nftType, bidAmount: Number(bid.price), wethBalance })
-                  repositories.bid.deleteById(bid.id)
-                }
-              }),
-            ])
-          },
-        )
-      }).then(() => true)
+              if (wethBalance < Number(bid.price)) {
+                logger.debug('softDeleteGenesisBid', { type: bid.nftType, bidAmount: Number(bid.price), wethBalance })
+                repositories.bid.deleteById(bid.id)
+              }
+            }),
+          ])
+        },
+      ).then(() => true)
   } catch (err) {
     console.log('error while validateLiveBalances: ', err)
   }
@@ -153,8 +150,17 @@ export const getEthereumEvents = (job: Job): Promise<any> => {
     //  * are bids for an available profile (search profiles for urls for status)
 
     const filter = { address: contract.address }
-    
-    return repositories.bid.find({ where: {} }).then((bids: entity.Bid[]) => Promise.all([
+
+    return repositories.bid.find({
+      where: [{
+        nftType: defs.NFTType.GenesisKey,
+        status: defs.BidStatus.Submitted,
+      },
+      {
+        nftType: defs.NFTType.Profile,
+        status: defs.BidStatus.Submitted,
+      }],
+    }).then((bids: entity.Bid[]) => Promise.all([
       filterLiveBids(bids),
       contract.queryFilter(filter),
     ])).then(([filteredBids, events]: [entity.Bid[], any[]]) => {
