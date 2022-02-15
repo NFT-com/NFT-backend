f318849b8bbcaf3fa117b8e9c16e6db9ed0d3d92
Gavin <gmaijoe@users.noreply.github.com>
2022-02-15 17:35:11 -0400

    Re-merge (#71)
    * feat: create marketplace ask, bid, swap entities
    
    * refactor: update marketplace ask, bid entities
    
    * fix: rename and re-org entities
    
    * fix: eth handler
    
    * fix: making sure nft jobs work
    
    * feat: added graphql schema
    
    * fix: merge in changes before adding more resolvers
    
    * fix: add in sentry_dsn
    
    * feat: add cron for syncing profiles to user id and wallet id if exists
    
    * fix: remove
    
    * fix: add 5 minute intervals
    
    * fix: remove for loop
    
    * fix: add abi
    
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>

--- packages/shared/src/helper/typechain/index.ts
+++ packages/shared/src/helper/typechain/index.ts
@@ -3,14 +3,14 @@
 /* eslint-disable */
 export type { ERC1155 } from "./ERC1155";
 export type { ERC721 } from "./ERC721";
+export type { NftProfile } from "./NftProfile";
 export type { GenesisKey } from "./GenesisKey";
 export type { ProfileAuction } from "./ProfileAuction";
-export type { NftProfile } from "./NftProfile";
 export type { Weth } from "./Weth";
 
 export { ERC1155__factory } from "./factories/ERC1155__factory";
 export { ERC721__factory } from "./factories/ERC721__factory";
+export { NftProfile__factory } from "./factories/NftProfile__factory";
 export { GenesisKey__factory } from "./factories/GenesisKey__factory";
 export { ProfileAuction__factory } from "./factories/ProfileAuction__factory";
 export { Weth__factory } from "./factories/Weth__factory";
-export { NftProfile__factory } from "./factories/NftProfile__factory";
