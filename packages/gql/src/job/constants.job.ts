569cc72cc3764ce3cdfc001386756d018c1b3597
Joey <joey@nft.com>
2022-03-10 08:39:37 -0800

    Feat/typesense mvp (#114)
    * feat: typesense initial mvp
    
    * feat: latest updates before merge main
    
    * feat: latest typesense updates
    
    * feat: update typesense env vars
    
    * feat: revert nft job time
    
    * feat: fix typesense env vars

--- packages/gql/src/job/constants.job.ts
+++ packages/gql/src/job/constants.job.ts
@@ -1,3 +1,4 @@
 export const NFT_COLLECTION_JOB = 'nft_collection'
 export const PROFILE_SYNC_JOB = 'profile_sync:4' // 4 = chainId
 export const MARKETPLACE_SYNC_JOB = 'marketplace_sync:4'
+export const TYPESENSE_INDEX_SCHEMA_JOB = 'typesense_index_schemas'
