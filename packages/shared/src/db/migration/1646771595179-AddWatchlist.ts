7c9a8abceb418170eee79f9a33c0641b4b3c061f
Jack Ta <jack.th.ta@gmail.com>
2022-03-11 13:13:34 -0800

    refactor: delete watchlistItem entity
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- packages/shared/src/db/migration/1646771595179-AddWatchlist.ts
+++ packages/shared/src/db/migration/1646771595179-AddWatchlist.ts
@@ -4,12 +4,10 @@ export class AddWatchlist1646771595179 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('CREATE TABLE "watchlist" ("id" character varying NOT NULL, "userId" character varying NOT NULL, "title" character varying NOT NULL)')
-    await queryRunner.query('CREATE TABLE "watchlist_item" ("id" character varying NOT NULL, "inWatchlist" character varying NOT NULL, "itemId" character varying NOT NULL)')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('DROP TABLE "watchlist"')
-    await queryRunner.query('DROP TABLE "watchlist_item"')
   }
 
 }
