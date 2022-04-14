e70ef4a2c9cfaa66c04354d35bd463181fc3f5c5
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-04-14 19:00:49 -0400

    fix: add sign

--- packages/gql/src/error/mint.error.ts
+++ packages/gql/src/error/mint.error.ts
@@ -1,8 +1,11 @@
 export enum ErrorType {
   WalletLosing = 'WALLET_LOSING',
-  ProfileClaimTransaction = 'PROFILE_CLAIM_TRANSATION'
+  ProfileClaimTransaction = 'PROFILE_CLAIM_TRANSATION',
+  WalletEmpty = 'WALLET_EMPTY',
 }
     
 export const buildWalletLosingMsg = (): string => 'This wallet does not have the winning bid for the profile you are trying to mint.'
     
 export const buildInvalidProfileClaimTransaction = (): string => 'This transaction does not show the input Wallet claiming the input Profile'
+
+export const buildWalletEmpty = (): string => 'There is not an associated wallet for this profile.'
