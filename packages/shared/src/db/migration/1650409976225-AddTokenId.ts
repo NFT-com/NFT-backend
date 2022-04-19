b4bfd48bd00fd3d4e1d981f17bb6a0619eecd17e
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-04-19 19:14:58 -0400

    fix: reset mig

--- packages/shared/src/db/migration/1650409976225-AddTokenId.ts
+++ packages/shared/src/db/migration/1650409976225-AddTokenId.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class AddTokenId1650408134211 implements MigrationInterface {
+export class AddTokenId1650409976225 implements MigrationInterface {
 
-  name = 'AddTokenId1650408134211'
+  name = 'AddTokenId1650409976225'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "profile" ADD "tokenId" character varying NOT NULL')
+    await queryRunner.query('ALTER TABLE "profile" ADD "tokenId" character varying')
     await queryRunner.query('ALTER TABLE "profile" ADD CONSTRAINT "UQ_059cbff1377f7aa85cca58b333f" UNIQUE ("tokenId")')
   }
 
