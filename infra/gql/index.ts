872152c2b46686a91830683123a03f93715628cc
Joey Punzel <joey@nft.com>
2022-04-21 09:28:29 -0400

    fix: drop alchemy nft endpoint (use normal)

--- infra/gql/index.ts
+++ infra/gql/index.ts
@@ -106,7 +106,6 @@ export const updateGQLEnvFile = (): void => {
   parsedFile['ZMOK_API_URL'] = process.env.ZMOK_API_URL || parsedFile['ZMOK_API_URL']
   parsedFile['ALCHEMY_API_KEY'] = process.env.ALCHEMY_API_KEY || parsedFile['ALCHEMY_API_KEY']
   parsedFile['INFURA_API_KEY'] = process.env.INFURA_API_KEY || parsedFile['INFURA_API_KEY']
-  parsedFile['ALCHEMY_NFT_API_URL'] = process.env.ALCHEMY_NFT_API_URL || parsedFile['ALCHEMY_NFT_API_URL']
   parsedFile['TYPESENSE_HOST'] = process.env.TYPESENSE_HOST || parsedFile['TYPESENSE_HOST']
   parsedFile['TYPESENSE_API_KEY'] = process.env.TYPESENSE_API_KEY || parsedFile['TYPESENSE_API_KEY']
 
