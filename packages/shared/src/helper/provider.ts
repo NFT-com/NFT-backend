ff037046ae6e018bbe90d78dd9478e03e1117451
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-04-24 21:51:42 -0400

    fix: ens base provider

--- packages/shared/src/helper/provider.ts
+++ packages/shared/src/helper/provider.ts
@@ -2,6 +2,6 @@ import { ethers, providers } from 'ethers'
 
 export const provider = (
   chainId: providers.Networkish = 1, //mainnet default
-): ethers.providers.Provider => {
+): ethers.providers.BaseProvider => {
   return new ethers.providers.JsonRpcProvider(process.env.ZMOK_API_URL, chainId)
 }
