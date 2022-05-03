373fb7a3455cfe312c85ddca3d8659f9394a0137
Jason Liu <jasonliudan@gmail.com>
2022-05-04 04:58:11 +0800

    feat: create composite image with profile url and avatar user uploaded (#222)

--- packages/gql/src/resolver/profile.resolver.ts
+++ packages/gql/src/resolver/profile.resolver.ts
@@ -12,7 +12,12 @@ import { Context, gql } from '@nftcom/gql/defs'
 import { appError, mintError, profileError } from '@nftcom/gql/error'
 import { auth, joi, pagination } from '@nftcom/gql/helper'
 import { core } from '@nftcom/gql/service'
-import { generateCompositeImage, getAWSConfig, s3ToCdn } from '@nftcom/gql/service/core.service'
+import {
+  DEFAULT_NFT_IMAGE,
+  generateCompositeImage,
+  getAWSConfig,
+  s3ToCdn,
+} from '@nftcom/gql/service/core.service'
 import { _logger, contracts, defs, entity, fp, helper, provider } from '@nftcom/shared'
 
 import { blacklistBool } from '../service/core.service'
@@ -408,7 +413,7 @@ const uploadProfileImages = async (
   ctx: Context,
 ): Promise<gql.Profile> => {
   const { repositories } = ctx
-  const { banner, avatar, profileId, description } = args.input
+  const { banner, avatar, profileId, description, compositeProfileURL } = args.input
   let profile = await repositories.profile.findById(profileId)
   if (!profile) {
     return Promise.reject(appError.buildNotFound(
@@ -475,9 +480,21 @@ const uploadProfileImages = async (
   if (avatarResponse && avatarStream) {
     const avatarUrl = await uploadStreamToS3(avatarResponse.filename, s3, avatarStream)
     if (avatarUrl) {
-      await repositories.profile.updateOneById(profileId, {
-        photoURL: avatarUrl,
-      })
+      // if user does not want to composite image with profile url, we just save image to photoURL
+      if (!compositeProfileURL) {
+        await repositories.profile.updateOneById(profileId, {
+          photoURL: avatarUrl,
+        })
+      }
+      // else, we will create composite image
+      else {
+        const compositeUrl = await generateCompositeImage(profile.url, avatarUrl)
+        if (compositeUrl) {
+          await repositories.profile.updateOneById(profileId, {
+            photoURL: compositeUrl,
+          })
+        }
+      }
     }
   }
 
@@ -504,7 +521,7 @@ const createCompositeImage = async (
     ))
   }
 
-  const imageURL = await generateCompositeImage(profile.url)
+  const imageURL = await generateCompositeImage(profile.url, DEFAULT_NFT_IMAGE)
   profile = await repositories.profile.updateOneById(profileId, {
     photoURL: imageURL,
   })
@@ -542,7 +559,7 @@ const createAllCompositeImages = async (
   if (!profiles.length) return true
   await Promise.allSettled(
     profiles.map(async (profile: entity.Profile) => {
-      const imageURL = await generateCompositeImage(profile.url)
+      const imageURL = await generateCompositeImage(profile.url, DEFAULT_NFT_IMAGE)
       await repositories.profile.updateOneById(
         profile.id,
         {
--- packages/gql/src/service/generateSVG.service.ts
+++ packages/gql/src/service/generateSVG.service.ts
@@ -1,7 +1,10 @@
 import { RubikBase64 } from './rubik.base64'
 import { RubikBlackBase64 } from './rubikBlack.base64'
 
-export const generateSVG = (profileURL: string): string => {
+export const generateSVG = (
+  profileURL: string,
+  defaultImagePath: string,
+): string => {
   return `<svg width="480" height="480"
   xmlns="http://www.w3.org/2000/svg">
   <defs>
@@ -16,7 +19,7 @@ export const generateSVG = (profileURL: string): string => {
       }
     </style>
   </defs>
-  <image href="https://cdn.nft.com/Medallion.jpg" height="480" width="480"/>
+  <image href="${defaultImagePath}" height="480" width="480"/>
   <text style="letter-spacing: 3px;" font-family="Rubik" x="50%" y="380" font-size="20"  text-anchor="middle" fill="#ccc">NFT.COM/</text>
   <text font-family="Rubik-Black" x="50%" y="420" font-size="30"  text-anchor="middle" fill="white">${profileURL}</text>
 </svg>`
