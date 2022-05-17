41d84dbdcd4956715bb7b338908b328a8241ccdd
Jason Liu <jasonliudan@gmail.com>
2022-05-17 12:00:39 -0700

    fix: add showNFTIds, hideNFTIds to update profile endpoint

--- packages/gql/src/resolver/profile.resolver.ts
+++ packages/gql/src/resolver/profile.resolver.ts
@@ -237,6 +237,8 @@ const updateProfile = (
     bannerURL: Joi.string().uri().allow(null),
     description: Joi.string().allow(null),
     photoURL: Joi.string().uri().allow(null),
+    showNFTIds: Joi.array().required().items(Joi.string()),
+    hideNFTIds: Joi.array().required().items(Joi.string()),
   })
   joi.validateSchema(schema, args.input)
 
