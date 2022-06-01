573baaa1d8097df1c1ec13fdf2faa2e649dc1abd
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2022-06-01 10:37:57 -0400

    fix: lambda handler for Bull job status (#244)
    * fix: lambda handler for Bull job status
    
    * fix: lambda handler for Bull job status
    
    * fix: restart queues after 250 invocations
    
    * fix: restart queues after 250 invocations
    
    * fix: drop obliterate from stopAndDisconnect
    
    * fix: promises not waiting for obliterate, so calling publish explicitly for chainId
    
    * chore: make obliterate repeat count configurable
    
    * chore: add env var to github actions and pulumi
    
    * fix: lambda handler for Bull job status
    
    * fix: lambda handler for Bull job status
    
    * fix: restart queues after 250 invocations
    
    * fix: restart queues after 250 invocations
    
    * fix: drop obliterate from stopAndDisconnect
    
    * fix: promises not waiting for obliterate, so calling publish explicitly for chainId
    
    * chore: make obliterate repeat count configurable
    
    * chore: add env var to github actions and pulumi
    
    * fix: only publish and li
    sten when necessary
    
    * fix: improve logs
    
    * chore: clean up commented out duplicate line

--- packages/gql/src/job/job.ts
+++ packages/gql/src/job/job.ts
@@ -15,6 +15,8 @@ import { generateCompositeImages } from '@nftcom/gql/job/profile.job'
 // import { syncMarketplace } from '@nftcom/gql/job/marketplace.job'
 // import { typesenseCollectionSchemas } from '@nftcom/gql/job/typesense.job'
 
+const BULL_MAX_REPEAT_COUNT = parseInt(process.env.BULL_MAX_REPEAT_COUNT) || 250
+
 const redis = {
   host: redisConfig.host,
   port: redisConfig.port,
@@ -22,7 +24,7 @@ const redis = {
 const queuePrefix = 'queue'
 const GENERATE_COMPOSITE_IMAGE = 'GENERATE_COMPOSITE_IMAGE'
 
-const queues: { [key: string]: Bull.Queue } = {}
+const queues = new Map<string, Bull.Queue>()
 
 const networkList = process.env.SUPPORTED_NETWORKS.replace('ethereum:', '').split(':')
 const networks = new Map()
@@ -31,67 +33,127 @@ networks.set(
   networkList[1], // human readable network name
 )
 
-const createQueues = (): void => {
-  networks.forEach((chainId: string, network: string) => {
-    queues[network] = new Bull(chainId, {
+let isListening: boolean
+
+const createQueues = (): Promise<void> => {
+  return new Promise((resolve) => {
+    networks.forEach((chainId: string, network: string) => {
+      queues.set(network, new Bull(chainId, {
+        prefix: queuePrefix,
+        redis,
+      }))
+    })
+    
+    // add composite image generation job to queue...
+    queues.set(GENERATE_COMPOSITE_IMAGE, new Bull(GENERATE_COMPOSITE_IMAGE, {
       prefix: queuePrefix,
       redis,
-    })
-  })
-  // add composite image generation job to queue...
-  queues[GENERATE_COMPOSITE_IMAGE] = new Bull(GENERATE_COMPOSITE_IMAGE, {
-    prefix: queuePrefix,
-    redis,
+    }))
+    resolve()
   })
 }
 
-const listenToJobs = (): Promise<void[]> => {
-  const values = Object.values(queues)
+const getExistingJobs = (): Promise<Bull.Job[][]> => {
+  const values = [...queues.values()]
   return Promise.all(values.map((queue) => {
-    switch (queue.name) {
-    case GENERATE_COMPOSITE_IMAGE:
-      return queue.process(generateCompositeImages)
-    default:
-      return queue.process(getEthereumEvents)
-    }
+    return queue.getJobs(['active', 'completed', 'delayed', 'failed', 'paused', 'waiting'])
   }))
 }
 
-const publishJobs = (): Promise<Bull.Job[]> => {
-  createQueues()
-  const chainIds = Object.keys(queues)
-  return Promise.all(chainIds.map((chainId) => {
-    switch (chainId) {
-    case GENERATE_COMPOSITE_IMAGE:
-      return queues[GENERATE_COMPOSITE_IMAGE].add({ GENERATE_COMPOSITE_IMAGE }, {
-        removeOnComplete: true,
-        removeOnFail: true,
-        // repeat every  2 minutes
-        repeat: { every: 2 * 60000 },
-        jobId: 'generate_composite_image',
-      })
-    default:
-      return queues[chainId].add({ chainId }, {
-        removeOnComplete: true,
-        removeOnFail: true,
-        // repeat every 10 minutes
-        repeat: { every: 10 * 60000 },
-        jobId: `chainid_${chainId}_job`,
-      })
+const jobHasNotRunRecently = (job: Bull.Job<any>): boolean  => {
+  const currentMillis = Date.now()
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: @types/bull is outdated
+  return currentMillis > (job.opts.repeat.every * 1.2) + job.opts.prevMillis
+}
+
+const checkJobQueues = (jobs: Bull.Job[][]): Promise<boolean> => {
+  const values = [...queues.values()]
+  if (jobs.flat().length < queues.size) {
+    console.log('🐮 fewer bull jobs than queues -- wiping queues for restart')
+    return Promise.all(values.map((queue) => {
+      return queue.obliterate({ force: true })
+    })).then(() => true)
+  }
+
+  for (const network of networks.keys()) {
+    const queue = queues.get(network)
+    const job = jobs.flat().find(job => job.queue === queue)
+    if ((job.opts.repeat
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore: @types/bull is outdated
+          && (job.opts.repeat.count >= BULL_MAX_REPEAT_COUNT || jobHasNotRunRecently(job)))
+        || !job.opts.repeat) {
+      console.log('🐮 bull job needs to restart -- wiping queues for restart')
+      return Promise.all(values.map((queue) => {
+        return queue.obliterate({ force: true })
+      })).then(() => true)
     }
-  }))
+  }
+
+  return new Promise(resolve => resolve(false))
+}
+
+const publishJobs = (shouldPublish: boolean): Promise<boolean> => {
+  if (shouldPublish) {
+    const chainIds = [...queues.keys()]
+    return Promise.all(chainIds.map((chainId) => {
+      switch (chainId) {
+      case GENERATE_COMPOSITE_IMAGE:
+        return queues.get(GENERATE_COMPOSITE_IMAGE).add({ GENERATE_COMPOSITE_IMAGE }, {
+          removeOnComplete: true,
+          removeOnFail: true,
+          // repeat every  2 minutes
+          repeat: { every: 2 * 60000 },
+          jobId: 'generate_composite_image',
+        })
+      default:
+        return queues.get(chainId).add({ chainId }, {
+          removeOnComplete: true,
+          removeOnFail: true,
+          // repeat every 10 minutes
+          repeat: { every: 10 * 60000 },
+          jobId: `chainid_${chainId}_job`,
+        })
+      }
+    })).then(() => shouldPublish)
+  }
+
+  return new Promise(resolve => resolve(false))
+}
+
+const listenToJobs = (shouldListen: boolean): Promise<boolean> => {
+  isListening = shouldListen
+  if (shouldListen) {
+    const values = [...queues.values()]
+    return Promise.all(values.map((queue) => {
+      switch (queue.name) {
+      case GENERATE_COMPOSITE_IMAGE:
+        return queue.process(generateCompositeImages)
+      default:
+        return queue.process(getEthereumEvents)
+      }
+    })).then(() => shouldListen)
+  }
+  
+  return new Promise(resolve => resolve(false))
 }
 
 export const startAndListen = (): Promise<void> => {
-  return publishJobs()
-    .then(() => void listenToJobs())
-    .then(() => console.log('🍊 listening for jobs...'))
+  return createQueues()
+    .then(() => getExistingJobs())
+    .then((jobs) => checkJobQueues(jobs))
+    .then((shouldPublish) => publishJobs(shouldPublish))
+    .then((shouldListen) => void listenToJobs(shouldListen))
+    .then(() => {
+      isListening ? console.log('🍊 queue was restarted -- listening for jobs...')
+        : console.log('🍊 bull jobs running healthy, no restart necessary')
+    })
 }
 
 export const stopAndDisconnect = (): Promise<any> => {
-  const values = Object.values(queues)
+  const values = [...queues.values()]
   return Promise.all(values.map((queue) => {
-    return queue.obliterate({ force: true })
-      .then(() => queue.close())
+    return queue.close()
   }))
 }
