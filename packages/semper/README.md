9ac6ec428b33d8e57699f421c043c13825212177
t-morgan <tim@immutableholdings.com>
2022-06-02 11:05:48 -0400

    feat: search engine tool

---
+++ packages/semper/README.md
@@ -0,0 +1,3 @@
+# S.E.M.P.E.R.
+
+Search Engine Maintenance, Provisioning, Erasing, and Restoring
--- packages/shared/src/helper/logger/types.ts
+++ packages/shared/src/helper/logger/types.ts
@@ -19,6 +19,7 @@ export enum LoggerContext {
   MarketAsk = 'marketAsk',
   MarketBid = 'marketBid',
   MarketSwap = 'marketSwap',
+  Typesense = 'typesense',
 }
 
 export enum LogLevel {
