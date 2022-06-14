8622938cef498d74a8084a433f531ef7f9ec07ae
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-06-14 11:39:33 -0400

    fix: test

--- packages/gql/src/resolver/marketAsk.resolver.ts
+++ packages/gql/src/resolver/marketAsk.resolver.ts
@@ -163,7 +163,6 @@ export const validAsk = async (
       throw Error(`provided signature ${JSON.stringify(marketAskArgs.input.signature)} doesn't match`)
     }
   } catch (err) {
-    logger.error('order validation error: ', err)
     Sentry.captureException(err)
     Sentry.captureMessage(`Order validation error: ${err}`)
     return false
