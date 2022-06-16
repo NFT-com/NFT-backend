9a915c62b684863fadca31af790053a506d80d8c
just-toby <ejd4@protonmail.com>
2022-06-16 12:45:50 -0700

    feat: wip unit test for profile endpoint

--- packages/gql/src/test/profile.unit.spec.ts
+++ packages/gql/src/test/profile.unit.spec.ts
@@ -8,15 +8,33 @@ describe('nft resolver', () => {
   describe('refresh nft endpoint', () => {
     let testServer
     beforeEach(async () => {
-      testServer = getTestApolloServer({})
+      testServer = getTestApolloServer({
+        profile: {
+          findByURL: (url: any) => Promise.resolve({
+            id: 'test',
+            url: url,
+            layoutType: 'Mosaic',
+          }),
+        },
+      })
     })
 
     afterEach(() => {
       jest.clearAllMocks()
     })
 
-    it('should accept profile layout type input', async () => {
-      console.log(testServer) // todo: write a test here.
+    it('should query profile by URL', async () => {
+      const result = await testServer.executeOperation({
+        query: `query Profile($url: String!) { 
+          profile(url: $url) { 
+            id 
+            layoutType 
+          } 
+        }`,
+        variables: { url: 'test' },
+      })
+            
+      expect(result.errors).toBeUndefined()
     })
   })
 })
