23861d368b8e9fd9c13dd861893101774f38c0a7
just toby <66155195+just-toby@users.noreply.github.com>
2022-06-17 10:44:08 -0700

    Feat/profile layout type (#277)
    * feat: stub api for getting 3rd party mktplace listings
    
    * fix: gql doc update for externallisting
    
    * fix: gql doc update for externallisting
    
    * fix: get opensea orders and offers from api
    
    * fix: add coverage into docs
    
    * fix: remove coverages from packages/gql
    
    * feat: update profile entity to store layout type
    
    * feat: wip unit test for profile endpoint
    
    * fix: update opensea best offer collection
    
    * feat: profile resolver integration test
    
    * feat: update profile entity to store layout type
    
    * feat: wip unit test for profile endpoint
    
    * feat: profile resolver integration test
    
    * fix: add payment token info to external listing
    
    * fix: add looksrare listing
    
    * feat: wip unit test for profile endpoint
    
    * feat: profile resolver integration test
    
    * fix: remove some files
    
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>
    Co-authored-by: github-ci/cd <engineering@nft.com>
    Co-authored-by: Gavin Mai <gavinmai@alumni.stanford.edu>

---
+++ packages/gql/src/test/profile.unit.spec.ts
@@ -0,0 +1,72 @@
+import { BigNumber } from 'ethers'
+
+const sharedLibs = jest.requireActual('@nftcom/shared')
+const { core } = jest.requireActual('@nftcom/gql/service')
+
+import { getTestApolloServer } from './util/testApolloServer'
+
+jest.setTimeout(20000)
+
+jest.mock('ioredis', () => jest.fn())
+jest.mock('@nftcom/gql/service', () => {
+  return {
+    core: {
+      ...core,
+      createProfileFromEvent: () => {
+        return {
+          id: 'testId',
+          createdAt: 0,
+          displayType: sharedLibs.defs.ProfileDisplayType.Collection,
+          layoutType: sharedLibs.defs.ProfileLayoutType.Mosaic,
+          url: 'test',
+        }
+      },
+    },
+  }
+})
+jest.mock('@nftcom/shared', () => {
+  return {
+    ...sharedLibs,
+    typechain: {
+      NftProfile__factory: {
+        connect: () => {
+          return {
+            getTokenId: () => Promise.resolve(BigNumber.from(1)),
+            ownerOf: () => Promise.resolve('0x0000000000000000000000000000000000000000'),
+          }
+        },
+      },
+    },
+  }
+})
+
+describe('profile resolver', () => {
+  describe('get profile endpoint', () => {
+    let testServer
+    beforeEach(async () => {
+      testServer = getTestApolloServer({
+        profile: {
+          findByURL: () => Promise.resolve(null),
+        },
+      })
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('should query profile by URL', async () => {
+      const result = await testServer.executeOperation({
+        query: `query Profile($url: String!) { 
+          profile(url: $url) { 
+            id 
+            layoutType 
+          } 
+        }`,
+        variables: { url: 'test' },
+      })
+            
+      expect(result.errors).toBeUndefined()
+    })
+  })
+})
