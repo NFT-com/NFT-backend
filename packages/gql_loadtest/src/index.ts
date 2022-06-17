9578fb8b85285a5635bad24e4968b3ba962ee7ce
t-morgan <tim@immutableholdings.com>
2022-06-17 13:06:27 -0400

    fix: setup args and use selected queries

--- packages/gql_loadtest/src/index.ts
+++ packages/gql_loadtest/src/index.ts
@@ -5,62 +5,16 @@ import path from 'path'
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeTypeDefs } from '@graphql-tools/merge'
 
+import * as args from './args.json'
+
 const typesArray = loadFilesSync(path.join(__dirname, '../../gql/src/schema'), { extensions: ['graphql'] })
 const typeDefs = mergeTypeDefs(typesArray)
 const printedTypeDefs = print(typeDefs)
 
-const args = {
-  myBids: {},
-  topBids: {},
-  collection: {
-    contract: {},
-    network: {},
-  },
-  myCurations: {
-    pageInput: {},
-  },
-  getAsks: {
-    makerAddress: {},
-    pageInput: {},
-  },
-  getNFTAsks: {
-    nftTokenId: 1,
-    nftContractAddress: {},
-  },
-  getNFTOffers: {
-    nftTokenId: 1,
-    nftContractAddress: {},
-  },
-  filterAsks: {},
-  getBids: {},
-  getSwaps: {},
-  getUserSwaps: {},
-  getContracts: {},
-  convertEnsToEthAddress: {},
-  gkNFTs: {},
-  nft: {},
-  nftById: {},
-  nfts: {},
-  myNFTs: {},
-  curationNFTs: {},
-  collectionNFTs: {},
-  profileFollowers: {},
-  profilesFollowedByMe: {},
-  myProfiles: {},
-  profile: {},
-  profilePassive: {},
-  blockedProfileURI: {},
-  insiderReservedProfiles: {},
-  latestProfiles: {},
-  me: {},
-  getMyGenesisKeys: {},
-  isAddressWhitelisted: {},
-  watchlist: {},
-}
-
 const easyGraphQLLoadTester = new LoadTesting(printedTypeDefs, args)
 
 easyGraphQLLoadTester.k6('k6.js', {
+  selectedQueries: ['collection', 'gkNFTs'],
   vus: 10,
   duration: '10s',
   queryFile: true,
