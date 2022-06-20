12628baaaf3941c110116f2f9fe444b4dc3d9c08
t-morgan <tim@immutableholdings.com>
2022-06-20 16:26:31 -0400

    chore: do not batch requests since 110 parallel requests per iteration seems excessive

--- packages/gql_loadtest/k6.js
+++ packages/gql_loadtest/k6.js
@@ -3,7 +3,7 @@ import http from 'k6/http'
 const queries = JSON.parse(open('./k6-gql-queries.json'))
 
 export const options = {
-  vus: 10,
+  vus: 20,
   duration: '30s',
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
@@ -12,12 +12,13 @@ export const options = {
 }
 
 export default function() {
-  const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
-  const params = { headers: { 'Content-Type': 'application/json' } }
-  http.batch(queries.map(query => {
-    return ["POST", url, JSON.stringify({
+  for (const query of queries) {
+    const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
+    const payload = JSON.stringify({
       query: query.query,
       variables: query.variables,
-    }), params]
-  }))
+    })
+    const params = { headers: { 'Content-Type': 'application/json' } }
+    http.post(url, payload, params)
+  }
 }
