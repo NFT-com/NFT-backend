0c6d51acc609dab90297e53e4cd278b7f80b1c9d
t-morgan <tim@immutableholdings.com>
2022-06-20 16:01:49 -0400

    chore: readme & thresholds

--- packages/gql_loadtest/README.md
+++ packages/gql_loadtest/README.md
@@ -1,2 +1,33 @@
 # NFT.com GQL Load Test
 
+## Run test locally
+
+At the moment, queries are only set up to run against dev
+
+```sh
+➜  export GQL_HOSTNAME=dev-api.nft.com
+➜  npm start
+```
+
+## Add Queries
+
+Add query name to list in `src/index.ts`:
+
+```ts
+const queries = easyGraphQLLoadTester.createQueries({ selectedQueries: [ ... ] })
+```
+
+Add query parameters to `src/args.json`... this is a json object with each of the query names for keys and 
+either an object containing the query parameters, or an array of objects supplying multiple different objects 
+containing the query parameters.
+
+```json
+{
+  "collection": {
+    "input": {
+      "contract": "0xbAE022721cfa57024781c3362724D5fBCE7443Da",
+      "network": "rinkeby"
+    }
+  }
+}
+```
--- packages/gql_loadtest/k6.js
+++ packages/gql_loadtest/k6.js
@@ -4,17 +4,20 @@ const queries = JSON.parse(open('./k6-gql-queries.json'))
 
 export const options = {
   vus: 10,
-  duration: '30s'
+  duration: '30s',
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
 }
 
 export default function() {
-  for (const query of queries) {
-    const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
-    const payload = JSON.stringify({
+  const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
+  const params = { headers: { 'Content-Type': 'application/json' } }
+  http.batch(queries.map(query => {
+    return ["POST", url, JSON.stringify({
       query: query.query,
       variables: query.variables,
-    })
-    const params = { headers: { 'Content-Type': 'application/json' } }
-    http.post(url, payload, params)
-  }
+    }), params]
+  }))
 }
