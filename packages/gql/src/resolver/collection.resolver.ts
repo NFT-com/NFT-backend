f4a426a543e3cb620bf0b00d430b749a1b1c5c37
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-06-22 15:59:18 -0400

    fix: abstracted

--- packages/gql/src/resolver/collection.resolver.ts
+++ packages/gql/src/resolver/collection.resolver.ts
@@ -1,15 +1,14 @@
-import axios from 'axios'
 import Redis from 'ioredis'
 
 import { redisConfig } from '@nftcom/gql/config'
 import { Context, gql } from '@nftcom/gql/defs'
+import {
+  retrieveCollectionOpensea,
+  retrieveCollectionStatsOpensea,
+} from '@nftcom/gql/service/opensea.service'
 import { _logger } from '@nftcom/shared'
 import * as Sentry from '@sentry/node'
 const logger = _logger.Factory(_logger.Context.Collection, _logger.Context.GraphQL)
-
-const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY
-const V1_OPENSEA_API_TESTNET_BASE_URL = 'https://testnets-api.opensea.io/api/v1'
-const V1_OPENSEA_API_BASE_URL = 'https://api.opensea.io/api/v1'
 
 const redis = new Redis({
   host: redisConfig.host,
@@ -33,32 +32,15 @@ const getCollection = async (
     if (cachedData) {
       return JSON.parse(cachedData)
     } else {
-      const baseUrl = args?.input?.chainId === '4' ? V1_OPENSEA_API_TESTNET_BASE_URL : V1_OPENSEA_API_BASE_URL
-
-      const config = args?.input?.chainId === '4' ? {
-        headers: { Accept: 'application/json' },
-      } :  {
-        headers: {
-          Accept: 'application/json',
-          'X-API-KEY': OPENSEA_API_KEY,
-        },
-      }
+      const data = await retrieveCollectionOpensea(args?.input?.contract, args?.input?.chainId)
+      let stats
   
-      let data, stats
-  
-      const url = `${baseUrl}/asset_contract/${args?.input?.contract}`
-      const res = await axios.get(url, config)
-  
-      if (res && res.data) {
-        data = res.data as gql.OpenseaContract
-  
+      if (data) {
         if (data?.collection?.slug) {
-          const statsUrl = `${baseUrl}/collection/${data?.collection?.slug}/stats`
-          const res2 = await axios.get(statsUrl, config)
-  
-          if (res2 && res2.data) {
-            stats = res2.data as gql.OpenseaStats
-          }
+          stats = await retrieveCollectionStatsOpensea(
+            data?.collection?.slug,
+            args?.input?.chainId,
+          )
         }
       }
 
