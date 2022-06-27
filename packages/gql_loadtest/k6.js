e70efd5877193e7e4921dd2cb9dfa3869941e7f4
t-morgan <tim@immutableholdings.com>
2022-06-27 14:55:37 -0400

    chore: use different thresholds for reads and writes

--- packages/gql_loadtest/k6.js
+++ packages/gql_loadtest/k6.js
@@ -7,7 +7,8 @@ export const options = {
   duration: '50s',
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+    "http_req_duration{rw:read}": ['p(95)<500'], // 95 percent of response times must be below 500ms
+    "http_req_duration{rw:write}": ['p(95)<3000'], // 95 percent of response times must be below 3s
   },
 }
 
@@ -18,12 +19,17 @@ export default function() {
       query: query.query,
       variables: query.variables,
     })
-    const params = { headers: {
-      'Content-Type': 'application/json',
-      'network': 'ethereum',
-      'chain-id': '4',
-      'authorization': '0x3e28a0402f9a3b2fa2c6ca855cf1335ce9637513af7e5e029f2b78d75b44c5f0233b479a16ca772efcf1574ba593195b8c1164df48825d146a2acef9badc52331c'
-    }}
-    http.post(url, payload, params)
+    const params = { 
+      headers: {
+        'Content-Type': 'application/json',
+        'network': 'ethereum',
+        'chain-id': '4',
+        'authorization': '0x3e28a0402f9a3b2fa2c6ca855cf1335ce9637513af7e5e029f2b78d75b44c5f0233b479a16ca772efcf1574ba593195b8c1164df48825d146a2acef9badc52331c'
+      },
+      tags: {
+        rw: query.contains('mutation') ? 'write' : 'read'
+      }
+  }
+    const res = http.post(url, payload, params)
   }
 }
