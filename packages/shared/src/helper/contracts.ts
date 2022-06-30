8e209ededd42db477688b02ad0fa6e2bdce7aab8
Jason Liu <jasonliudan@gmail.com>
2022-06-30 12:09:13 -0700

    fix: update contracts for goerli

--- packages/shared/src/helper/contracts.ts
+++ packages/shared/src/helper/contracts.ts
@@ -14,10 +14,10 @@ import validationLogicABIJSON from '@nftcom/shared/helper/abis/ValidationLogic.j
 // TODO: move contract addresses to Doppler
 export function nftTokenAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0xd60054F74c9685e5F9E474F36344494D6a1DB3cF')
+  case 5:
+  case '5':
+  case 'goerli':
+    return utils.getAddress('0x7ffe04f3213d893bb4ebe76fbb49ca2a8f9c4610')
   case '1':
   case 1:
   case 'mainnet':
@@ -29,10 +29,10 @@ export function nftTokenAddress(chainId: string | number = 'mainnet'): string {
 // deployedGkTeamDistributor
 export function genesisKeyTeamMerkleAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
-    return utils.getAddress('0x7a546F59e04Fff0b5eD3Ee13e30F38917C74741B')
+  case '5':
+  case 'goerli':
+  case 5:
+    return utils.getAddress('0x0000000000000000000000000000000000000000')
   case '1':
   case 1:
   case 'mainnet':
@@ -42,10 +42,10 @@ export function genesisKeyTeamMerkleAddress(chainId: string | number = 'mainnet'
 
 export function genesisKeyTeamClaimAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
-    return utils.getAddress('0x41E3E44e2Db9fFC7b69CF459441C80F95Cb25fCc')
+  case '5':
+  case 'goerli':
+  case 5:
+    return utils.getAddress('0x4a76adbfF8aA29e0B6E051660119768e0f870557')
   case '1':
   case 1:
   case 'mainnet':
@@ -55,10 +55,10 @@ export function genesisKeyTeamClaimAddress(chainId: string | number = 'mainnet')
 
 export function genesisKeyAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
-    return utils.getAddress('0x530E404f51778F38249413264ac7807A16b88603')
+  case '5':
+  case 'goerli':
+  case 5:
+    return utils.getAddress('0xe0060010c2c81A817f4c52A9263d4Ce5c5B66D55')
   case '1':
   case 1:
   case 'mainnet':
@@ -68,10 +68,10 @@ export function genesisKeyAddress(chainId: string | number = 'mainnet'): string 
 
 export function genesisKeyStakeAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
-    return utils.getAddress('0xe1f5466cbd61652D792E4A5c07D0F59E6c3d360a')
+  case '5':
+  case 'goerli':
+  case 5:
+    return utils.getAddress('0x0000000000000000000000000000000000000000')
   case '1':
   case 1:
   case 'mainnet':
@@ -81,10 +81,10 @@ export function genesisKeyStakeAddress(chainId: string | number = 'mainnet'): st
 
 export function nftProfileAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0x7e229a305f26ce5C39AAB1d90271e1Ef03d764D5')
+  case 5:
+  case '5':
+  case 'goerli':
+    return utils.getAddress('0x073272c91A741E453aE47c10Be2F7ab5131B0706')
   case '1':
   case 1:
   case 'mainnet':
@@ -95,10 +95,10 @@ export function nftProfileAddress(chainId: string | number = 'mainnet'): string 
 
 export function profileAuctionAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
-    return utils.getAddress('0x1338A9ec2Ef9906B57082dB0F67ED9E6E661F4A7')
+  case '5':
+  case 'goerli':
+  case 5:
+    return utils.getAddress('0x40023d97Ca437B966C8f669C91a9740C639E21C3')
   case '1':
   case 1:
   case 'mainnet':
@@ -108,10 +108,10 @@ export function profileAuctionAddress(chainId: string | number = 'mainnet'): str
 
 export function validationLogicAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0x10CF5F4C7cEeBb483A64E9500816f7356dC9379a')
+  case 5:
+  case '5':
+  case 'goerli':
+    return utils.getAddress('0x0000000000000000000000000000000000000000')
   case '1':
   case 1:
   case 'mainnet':
@@ -122,10 +122,10 @@ export function validationLogicAddress(chainId: string | number = 'mainnet'): st
 
 export function marketplaceEventAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0x0Fae273409Ad108692d6515dd134A338364f1689')
+  case 5:
+  case '5':
+  case 'goerli':
+    return ''
   case '1':
   case 1:
   case 'mainnet':
@@ -136,10 +136,10 @@ export function marketplaceEventAddress(chainId: string | number = 'mainnet'): s
 
 export function nftMarketplaceAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0x4dCF39912385A00ed722ce0b70A21d616eA6BAEC')
+  case 5:
+  case '5':
+  case 'goerli':
+    return ''
   case '1':
   case 1:
   case 'mainnet':
@@ -150,10 +150,10 @@ export function nftMarketplaceAddress(chainId: string | number = 'mainnet'): str
 
 export function wethAddress(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return utils.getAddress('0xc778417E063141139Fce010982780140Aa0cD5Ab')
+  case 5:
+  case '5':
+  case 'goerli':
+    return utils.getAddress('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6')
   case '1':
   case 1:
   case 'mainnet':
@@ -170,10 +170,10 @@ interface Options {
 // https://github.com/wbobeirne/eth-balance-checker (multi balance contract)
 export function multiBalance(chainId: string | number = 'mainnet'): Options {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return { contractAddress: utils.getAddress('0x3183B673f4816C94BeF53958BaF93C671B7F8Cf2') }
+  case 5:
+  case '5':
+  case 'goerli':
+    return { contractAddress: utils.getAddress('0x9788C4E93f9002a7ad8e72633b11E8d1ecd51f9b') }
   case '1':
   case 1:
   case 'mainnet':
@@ -185,9 +185,9 @@ export function multiBalance(chainId: string | number = 'mainnet'): Options {
 // blind whitelist winners
 export function genesisKeyDistributor(chainId: string | number = 'mainnet'): string {
   switch (chainId) {
-  case '4':
-  case 'rinkeby':
-  case 4:
+  case '5':
+  case 'goerli':
+  case 5:
     return utils.getAddress('')
   case '1':
   case 1:
@@ -277,10 +277,10 @@ export function getEthGasInfo(chainId: number): Promise<GasInfo> {
 
 export function getProfileAuctionMnemonic(chainId: string | number): string {
   switch (chainId) {
-  case 4:
-  case '4':
-  case 'rinkeby':
-    return process.env.MNEMONIC_RINKEBY
+  case 5:
+  case '5':
+  case 'goerli':
+    return process.env.MNEMONIC_GOERLI
   case '1':
   case 1:
   case 'mainnet':
