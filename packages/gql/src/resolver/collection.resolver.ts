5e2df80251a9de0c0612956f140449889ef977be
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-06-30 11:39:43 -0400

    fix: add cache

--- packages/gql/src/resolver/collection.resolver.ts
+++ packages/gql/src/resolver/collection.resolver.ts
@@ -22,35 +22,38 @@ const getCollection = async (
 ): Promise<gql.CollectionInfo> => {
   try {
     logger.debug('getCollection', { input: args?.input })
-    const key = `${args?.input?.contract?.toLowerCase()}-${args?.input?.chainId}`
+    const key = `${args?.input?.contract?.toLowerCase()}-${args?.input?.chainId}-${args?.input?.withOpensea ? 'true' : 'false'}`
     const cachedData = await redis.get(key)
 
     if (cachedData) {
       return JSON.parse(cachedData)
     } else {
       let stats, data
-      const slugKey = `${key}-slug`
-      const cachedData = JSON.parse(await redis.get(slugKey))
 
-      if (cachedData?.collection?.slug) {
-        data = cachedData
-        stats = await retrieveCollectionStatsOpensea(
-          cachedData?.collection?.slug,
-          args?.input?.chainId,
-        )
-      } else {
-        data = await retrieveCollectionOpensea(args?.input?.contract, args?.input?.chainId)
+      if (args?.input?.withOpensea) {
+        const slugKey = `${key}-slug`
+        const cachedData = JSON.parse(await redis.get(slugKey))
   
-        if (data) {
-          if (data?.collection?.slug) {
-            stats = await retrieveCollectionStatsOpensea(
-              data?.collection?.slug,
-              args?.input?.chainId,
-            )
+        if (cachedData?.collection?.slug) {
+          data = cachedData
+          stats = await retrieveCollectionStatsOpensea(
+            cachedData?.collection?.slug,
+            args?.input?.chainId,
+          )
+        } else {
+          data = await retrieveCollectionOpensea(args?.input?.contract, args?.input?.chainId)
+    
+          if (data) {
+            if (data?.collection?.slug) {
+              stats = await retrieveCollectionStatsOpensea(
+                data?.collection?.slug,
+                args?.input?.chainId,
+              )
+            }
           }
+  
+          await redis.set(slugKey, JSON.stringify(data)) // set cache
         }
-
-        await redis.set(slugKey, JSON.stringify(data)) // set cache
       }
 
       const returnObject = {
