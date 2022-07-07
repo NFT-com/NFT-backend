401a1dde3a07dc3a9a608c21e1b56140a10fe621
t-morgan <tim@immutableholdings.com>
2022-07-07 08:07:17 -0400

    fix: only set doppler for local vs code run

--- packages/gql/jest.config.js
+++ packages/gql/jest.config.js
@@ -1,8 +1,10 @@
-require('child_process').execSync('doppler run -- printenv').toString().split('\n').reduce((acc, envStr) => {
-  const name = envStr.split('=')[0];
-  acc[name] = envStr.substring(name.length + 1);
-  return acc;
-}, process.env);
+if (process.env.VS_CODE === '1') {
+  require('child_process').execSync('doppler run -- printenv').toString().split('\n').reduce((acc, envStr) => {
+    const name = envStr.split('=')[0];
+    acc[name] = envStr.substring(name.length + 1);
+    return acc;
+  }, process.env);
+}
 
 module.exports = {
   preset: 'ts-jest',
