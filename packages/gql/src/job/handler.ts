3bf6141e5ba41d09b4074327f2fb88f20fe91474
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-07-15 14:56:51 -0400

    fix: user logger

--- packages/gql/src/job/handler.ts
+++ packages/gql/src/job/handler.ts
@@ -284,7 +284,7 @@ export const getEthereumEvents = async (job: Job): Promise<any> => {
           }
         }
       } catch (err) {
-        console.log('error parsing resolver: ', err.response)
+        logger.error('error parsing resolver: ', err)
       }
     })
 
