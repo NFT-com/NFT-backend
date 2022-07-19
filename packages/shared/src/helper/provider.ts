ddaf7ca52b3e40b17c48b3d5c14eb15093167917
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-07-19 18:18:57 -0400

    fix: console

--- packages/shared/src/helper/provider.ts
+++ packages/shared/src/helper/provider.ts
@@ -1,4 +1,4 @@
-import { ethers, logger, providers } from 'ethers'
+import { ethers, providers } from 'ethers'
 
 export const provider = (
   chainId: providers.Networkish = 1, //mainnet default
@@ -6,9 +6,9 @@ export const provider = (
   const ALCHEMY_API_URL = Number(chainId) == 1 ? (process.env.ALCHEMY_API_URL).replace('https://eth-mainnet.alchemyapi.io/v2/', '') :
     Number(chainId) == 5 ? (process.env.ALCHEMY_API_URL_GOERLI).replace('https://eth-goerli.g.alchemy.com/v2/', '') : (process.env.ALCHEMY_API_URL_RINKEBY).replace('https://eth-rinkeby.alchemyapi.io/v2/', '')
 
-  logger.debug('*** process.env.ALCHEMY_API_URL: ', process.env.ALCHEMY_API_URL)
-  logger.debug('*** process.env.ALCHEMY_API_URL_RINKEBY: ', process.env.ALCHEMY_API_URL_RINKEBY)
-  logger.debug('*** process.env.ALCHEMY_API_URL_GOERLI: ', process.env.ALCHEMY_API_URL_GOERLI)
-  logger.debug('*** ALCHEMY_API_URL: ', ALCHEMY_API_URL)
+  console.log('*** process.env.ALCHEMY_API_URL: ', process.env.ALCHEMY_API_URL)
+  console.log('*** process.env.ALCHEMY_API_URL_RINKEBY: ', process.env.ALCHEMY_API_URL_RINKEBY)
+  console.log('*** process.env.ALCHEMY_API_URL_GOERLI: ', process.env.ALCHEMY_API_URL_GOERLI)
+  console.log('*** ALCHEMY_API_URL: ', ALCHEMY_API_URL)
   return new ethers.providers.AlchemyProvider(chainId, ALCHEMY_API_URL)
 }
