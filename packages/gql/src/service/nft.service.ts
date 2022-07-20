366b76a23faa8e885859f1c2b112ab6ce03efc58
Jason Liu <jasonliudan@gmail.com>
2022-07-20 12:38:09 -0700

    fix: create clearGKIconVisible endpoint

--- packages/gql/src/service/nft.service.ts
+++ packages/gql/src/service/nft.service.ts
@@ -599,6 +599,26 @@ export const refreshNFTMetadata = async (
   }
 }
 
+export const getOwnersOfGenesisKeys = async (
+  contract: string,
+  chainId: string,
+): Promise<string[]> => {
+  if (chainId !== '1' && chainId !== '5') return []
+  try {
+    // until Alchemy SDK incorporates this
+    // TODO: remove in future
+    const alchemy_api_url = chainId === '1' ? process.env.ALCHEMY_API_URL : process.env.ALCHEMY_API_URL_GOERLI
+    const res = await axios.get(`${alchemy_api_url}/getOwnersForCollection?contractAddress=${contract}`)
+    if (res && res.data && res.data.ownerAddresses) {
+      return res.data.ownerAddresses as string[]
+    } else {
+      return []
+    }
+  } catch (err) {
+    Sentry.captureMessage(`Error in getOwnersOfGenesisKeys: ${err}`)
+  }
+}
+
 const hideAllNFTs = async (
   repositories: db.Repository,
   profileId: string,
