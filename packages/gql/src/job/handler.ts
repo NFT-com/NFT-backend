1d2d053c456ccba34c8507bd087d3dbd06ad50ba
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-07-25 17:36:22 -0400

    fix: remove unnecessary helper

--- packages/gql/src/job/handler.ts
+++ packages/gql/src/job/handler.ts
@@ -1,12 +1,10 @@
 import { Job } from 'bull'
-import { getAddressesBalances } from 'eth-balance-checker/lib/ethers'
-import { BigNumber, ethers, utils } from 'ethers'
-import * as Lodash from 'lodash'
+import { ethers, utils } from 'ethers'
 
 import { provider } from '@nftcom/gql/helper'
 import { getPastLogs } from '@nftcom/gql/job/marketplace.job'
 import { cache } from '@nftcom/gql/service/cache.service'
-import { _logger, contracts, db, defs, entity, helper } from '@nftcom/shared'
+import { _logger, contracts, db, defs, helper } from '@nftcom/shared'
 import * as Sentry from '@sentry/node'
 
 import { core } from '../service'
@@ -21,100 +19,6 @@ type Log = {
   latestBlockNumber: number
 }
 
-const onlyUnique = (value, index, self: any[]): boolean => {
-  return self.indexOf(value) === index
-}
-
-// size of each array for balances for ETH
-// 900 balanceOf queries maxes sits comfortably below the 1000 maximum size limit on Ethereum
-// the size limit is mostly due to gas limits per ethereum call.
-//
-// Even though balanceOf is a view function, it has to conform to size limits on calls via the
-// ethereum nodes
-const perChunk = 900
-
-const getAddressBalanceMapping = async (
-  bids: entity.Bid[],
-  walletIdAddressMapping: any,
-  chainId: number,
-) : Promise<[entity.Bid[], any, any]> => {
-  const splitAddressArrays: string[][] = Lodash.chunk(
-    Object.values(walletIdAddressMapping),
-    perChunk,
-  )
-
-  const genesisKeyBids = bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.GenesisKey)
-  const balanceArrays = []
-  await Promise.allSettled(
-    splitAddressArrays.map(async (splitArray: string[]) => {
-      const balances = await getAddressesBalances(
-        provider.provider(Number(chainId)),
-        splitArray,
-        [ethers.constants.AddressZero],
-        contracts.multiBalance(chainId),
-      )
-      balanceArrays.push(balances)
-    }),
-  )
-  const addressBalanceMapping = Object.assign({}, ...balanceArrays)
-  return [genesisKeyBids, walletIdAddressMapping, addressBalanceMapping]
-}
-
-// validates live balances for all the filtered bids
-const validateLiveBalances = (bids: entity.Bid[], chainId: number): Promise<boolean> => {
-  try {
-    return Promise.all([
-      bids,
-      bids
-        .map(bid => bid.walletId)
-        .filter(onlyUnique).map((walletId: string) => { return { id: walletId }}),
-    ]).then(
-      ([bids, uniqueWalletIds]:
-      [entity.Bid[], Array<{ id: string }>]) => {
-        return Promise.all([
-          bids,
-          repositories.wallet.find({ where: uniqueWalletIds }).then((wallets: entity.Wallet[]) =>  {
-            // create mapping of walletId => address
-            return wallets.reduce((map, walletEntity) =>
-              (map[walletEntity.id] = walletEntity.address, map), {})
-          }),
-        ])
-      }).then(
-      ([
-        bids,
-        walletIdAddressMapping,
-      ]: [entity.Bid[], any]) => getAddressBalanceMapping(bids, walletIdAddressMapping, chainId))
-      .then(
-        ([
-          genesisKeyBids,
-          walletIdAddressMapping,
-          addressBalanceMapping,
-        ]: [entity.Bid[], any, any]) => {
-          return Promise.all([
-            genesisKeyBids.map(async (bid: entity.Bid) => {
-              try {
-                const balanceObj = addressBalanceMapping[walletIdAddressMapping[bid.walletId]]
-                const ethBalance = BigNumber.from(balanceObj[ethers.constants.AddressZero]) ??
-                  BigNumber.from(0)
-                if (ethBalance.lt(BigNumber.from(bid.price))
-                  && new Date().getTime() < 1651186800000
-                ) {
-                  logger.info('softDeleteGenesisBid', { type: bid.nftType, bidAmount: Number(bid.price), ethBalance })
-                  repositories.bid.deleteById(bid.id)
-                }
-              } catch (err) {
-                logger.debug('gk balance: ', err)
-
-                Sentry.captureMessage(`gk balance error in validateLiveBalances: ${err}`)
-              }
-            }),
-          ])
-        },
-      ).then(() => true)
-  } catch (err) {
-    Sentry.captureMessage(`Error in validateLiveBalances: ${err}`)
-  }
-}
 const profileAuctionInterface = new utils.Interface(contracts.profileAuctionABI())
 const nftResolverInterface = new utils.Interface(contracts.NftResolverABI())
 
@@ -236,13 +140,6 @@ export const getEthereumEvents = async (job: Job): Promise<any> => {
 
     logger.debug(`👾 getting Ethereum Events chainId=${chainId}`)
 
-    const bids = await repositories.bid.find({
-      where: [{
-        nftType: defs.NFTType.GenesisKey,
-        status: defs.BidStatus.Submitted,
-      }],
-    })
-    const filteredBids = bids.filter((bid: entity.Bid) => bid.nftType == defs.NFTType.GenesisKey)
     const log = await getMintedProfileEvents(topics, Number(chainId), chainProvider, address)
     const log2 = await getResolverEvents(
       topics2,
@@ -351,71 +248,69 @@ export const getEthereumEvents = async (job: Job): Promise<any> => {
       }
     })
 
-    logger.debug('filterLiveBids', { filteredBids: filteredBids.map(i => i.id) })
-    const validation = await validateLiveBalances(filteredBids, chainId)
-    if (validation) {
-      await Promise.allSettled(
-        log.logs.map(async (unparsedEvent) => {
-          const evt = profileAuctionInterface.parseLog(unparsedEvent)
-          logger.info(`Found event MintedProfile with chainId: ${chainId}, ${evt.args}`)
-          const [owner,profileUrl,tokenId,,] = evt.args
+    log.logs.map(async (unparsedEvent) => {
+      try {
+        const evt = profileAuctionInterface.parseLog(unparsedEvent)
+        logger.info(`Found event MintedProfile with chainId: ${chainId}, ${evt.args}`)
+        const [owner,profileUrl,tokenId,,] = evt.args
 
-          if (evt.name === 'MintedProfile') {
-            const existsBool = await repositories.event.exists({
-              chainId,
-              contract: helper.checkSum(contracts.profileAuctionAddress(chainId)),
-              eventName: evt.name,
-              txHash: unparsedEvent.transactionHash,
-              ownerAddress: owner,
-              profileUrl: profileUrl,
+        if (evt.name === 'MintedProfile') {
+          const existsBool = await repositories.event.exists({
+            chainId,
+            contract: helper.checkSum(contracts.profileAuctionAddress(chainId)),
+            eventName: evt.name,
+            txHash: unparsedEvent.transactionHash,
+            ownerAddress: owner,
+            profileUrl: profileUrl,
+          })
+          if (!existsBool) {
+            await repositories.event.save(
+              {
+                chainId,
+                contract: helper.checkSum(contracts.profileAuctionAddress(chainId)),
+                eventName: evt.name,
+                txHash: unparsedEvent.transactionHash,
+                ownerAddress: owner,
+                profileUrl: profileUrl,
+              },
+            )
+            // find and mark profile status as minted
+            const profile = await repositories.profile.findOne({
+              where: {
+                tokenId: tokenId.toString(),
+                url: profileUrl,
+                chainId,
+              },
             })
-            if (!existsBool) {
-              await repositories.event.save(
-                {
-                  chainId,
-                  contract: helper.checkSum(contracts.profileAuctionAddress(chainId)),
-                  eventName: evt.name,
-                  txHash: unparsedEvent.transactionHash,
-                  ownerAddress: owner,
-                  profileUrl: profileUrl,
-                },
+            if (!profile) {
+              await core.createProfileFromEvent(
+                chainId,
+                owner,
+                tokenId,
+                repositories,
+                profileUrl,
+                true,
               )
-              // find and mark profile status as minted
-              const profile = await repositories.profile.findOne({
-                where: {
-                  tokenId: tokenId.toString(),
-                  url: profileUrl,
-                  chainId,
-                },
-              })
-              if (!profile) {
-                await core.createProfileFromEvent(
-                  chainId,
-                  owner,
-                  tokenId,
-                  repositories,
-                  profileUrl,
-                  true,
-                )
 
-                logger.debug(`Profile ${ profileUrl } was minted by address ${ owner }`)
-                await HederaConsensusService.submitMessage(
-                  `Profile ${ profileUrl } was minted by address ${ owner }`,
-                )
-              } else {
-                if (profile.status !== defs.ProfileStatus.Owned) {
-                  await repositories.profile.updateOneById(profile.id, {
-                    status: defs.ProfileStatus.Owned,
-                  })
-                }
+              logger.debug(`Profile ${ profileUrl } was minted by address ${ owner }`)
+              await HederaConsensusService.submitMessage(
+                `Profile ${ profileUrl } was minted by address ${ owner }`,
+              )
+            } else {
+              if (profile.status !== defs.ProfileStatus.Owned) {
+                await repositories.profile.updateOneById(profile.id, {
+                  status: defs.ProfileStatus.Owned,
+                })
               }
             }
           }
-        }),
-      )
-      await cache.set(chainIdToCacheKeyProfile(chainId), log.latestBlockNumber)
-      logger.debug('saved all minted profiles and their events', { counts: log.logs.length })
-    }
+        }
+        await cache.set(chainIdToCacheKeyProfile(chainId), log.latestBlockNumber)
+        logger.debug('saved all minted profiles and their events', { counts: log.logs.length })
+      } catch (err) {
+        logger.error('error parsing minted profiles: ', err)
+      }
+    })
   } catch (err) {
     Sentry.captureMessage(`Error in getEthereumEvents Job: ${err}`)
   }
