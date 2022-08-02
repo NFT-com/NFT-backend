9577add9915ebdf42107c5da7701872e37f0f82a
t-morgan <tim@immutableholdings.com>
2022-08-02 10:36:36 -0400

    fix: use profile id since it is different than nft id

--- packages/gql/src/service/nft.service.ts
+++ packages/gql/src/service/nft.service.ts
@@ -500,12 +500,20 @@ const updateNFTOwnershipAndMetadata = async (
     } else {
       // if this NFT is existing and owner changed, we change its ownership...
       if (existingNFT.userId !== userId || existingNFT.walletId !== walletId) {
-        const previousWallet = await repositories.wallet.findById(existingNFT.walletId)
-        // if this NFT was previous owner's preferred profile
-        if (existingNFT.id === previousWallet.profileId) {
-          await repositories.wallet.updateOneById(previousWallet.id, {
-            profileId: null,
-          })
+        // if this NFT is a profile NFT...
+        if (existingNFT.contract === contracts.nftProfileAddress(chainId)) {
+          const previousWallet = await repositories.wallet.findById(existingNFT.walletId)
+          const profile = await repositories.profile.findOne({ where: {
+            tokenId: BigNumber.from(existingNFT.tokenId).toString(),
+            walletId: previousWallet.id,
+            userId: previousWallet.userId,
+          } })
+          // if this NFT was previous owner's preferred profile...
+          if (profile.id === previousWallet.profileId) {
+            await repositories.wallet.updateOneById(previousWallet.id, {
+              profileId: null,
+            })
+          }
         }
         return await repositories.nft.updateOneById(existingNFT.id, {
           userId,
