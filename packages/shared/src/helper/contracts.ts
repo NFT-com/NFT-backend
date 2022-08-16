0b848820a247ef7e5cdd707e79057bdbe6fd33ce
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-08-16 13:52:19 -0400

    fix: updated mainnet contract address

--- packages/shared/src/helper/contracts.ts
+++ packages/shared/src/helper/contracts.ts
@@ -132,7 +132,7 @@ export function nftResolverAddress(chainId: string | number = 'mainnet'): string
   case 1:
   case 'mainnet':
   default:
-    return utils.getAddress('0xe9F5CBeE4a58B1EB47b303c14765Ab102E4ABC37')
+    return utils.getAddress('0xA657C988e8aC39D3268D390eB7c522a535B10453')
   }
 }
 
