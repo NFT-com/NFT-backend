3b56739e4bd5662f16d4b23a17c26df899846b96
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-08-18 13:41:41 -0400

    fix: duplicate events

--- packages/shared/src/db/migration/1660843992119-uniqueEventConstraint.ts
+++ packages/shared/src/db/migration/1660843992119-uniqueEventConstraint.ts
@@ -5,13 +5,11 @@ export class uniqueEventConstraint1660843992119 implements MigrationInterface {
   name = 'uniqueEventConstraint1660843992119'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "event" DROP CONSTRAINT "UQ_EVENT"')
     await queryRunner.query('ALTER TABLE "event" ADD CONSTRAINT "UQ_e792beb1fe09fa525ee9a1e679c" UNIQUE ("txHash", "profileUrl")')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "event" DROP CONSTRAINT "UQ_e792beb1fe09fa525ee9a1e679c"')
-    await queryRunner.query('ALTER TABLE "event" ADD CONSTRAINT "UQ_EVENT" UNIQUE ("txHash", "profileUrl")')
   }
 
 }
