4849cf8ef8ac7e2b94f8d1c59fcd708640da54dd
Jason Liu <jasonliudan@gmail.com>
2022-08-29 11:12:29 -0700

    fix: update migration

--- packages/shared/src/db/migration/1661450639658-UpdateNFTEntity.ts
+++ packages/shared/src/db/migration/1661450639658-UpdateNFTEntity.ts
@@ -1,8 +1,8 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class UpdateNFTEntity1661450639656 implements MigrationInterface {
+export class UpdateNFTEntity1661450639658 implements MigrationInterface {
 
-  name = 'UpdateNFTEntity1661450639656'
+  name = 'UpdateNFTEntity1661450639658'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "nft" ADD "previewLink" character varying')
