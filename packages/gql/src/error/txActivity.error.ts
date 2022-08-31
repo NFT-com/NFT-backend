d25644b177bc96ecea408c73b721e9be2da9cac3
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-08-31 11:02:08 -0400

    feat: update status

--- packages/gql/src/error/txActivity.error.ts
+++ packages/gql/src/error/txActivity.error.ts
@@ -1,6 +1,8 @@
 export enum ErrorType {
   ActivityNotSet = 'ACTIVITY_NOT_SENT',
   ActivityIncorrect = 'ACTIVITY_INCORRECT',
+  StatusIncorrect = 'STATUS_INCORRECT',
+  StatusNotAllowed = 'STATUS_NOT_ALLOWED',
   NoActivityToUpdate = 'NO_ACTIVITY_TO_UPDATE',
   TokenWithNoContract = 'TOKEN_NO_CONTRACT',
   CollectionNotSupported = 'COLLECTION_NOT_SUPPORTED',
@@ -11,7 +13,13 @@ export const buildNoActivityId = (): string => 'No Activity Ids Sent'
 
 export const buildIncorrectActivity = (activity: string): string => `Activity ${activity} is incorrect`
 
-export const buildNoActivitiesToUpdate = (): string => 'Activities are not owned by wallet or are already read'
+export const buildIncorrectStatus = (status: string): string => `Status ${status} is incorrect`
+
+export const buildStatusNotAllowed = (status: string): string => `Status ${status} is not allowed for this operation`
+
+export const buildNoActivitiesReadToUpdate = (): string => 'Activities are not owned by wallet or are already read'
+
+export const buildNoActivitiesStatusToUpdate = (): string => 'Activities are not owned by wallet'
 
 export const buildTokenWithNoContract = (): string => 'Token Id is sent without any contract'
 
