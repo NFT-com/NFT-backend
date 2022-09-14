3263168c19f493717f9910314c4b5466158f0500
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-09-14 15:49:24 -0400

    feat: clean up

--- packages/gql/src/test/resolver/approval.resolver.int.spec.ts
+++ packages/gql/src/test/resolver/approval.resolver.int.spec.ts
@@ -36,6 +36,8 @@ jest.mock('@nftcom/gql/service/cache.service', () => ({
   createCacheConnection: jest.fn(),
 }))
 
+jest.mock('@nftcom/gql/service/nft.service')
+
 jest.mock('@nftcom/gql/service', () => {
   return {
     core: {
