efbbf9a3234cf0c3d43d329ba83d0a582b22e874
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2022-10-07 12:25:22 -0400

    Feat/sales processor (#573)
    * feat: add erc20 columns
    
    * feat: cache erc20 symbols
    
    * fix: typescript doesn't really like typechain that much
    
    * chore: add test
    
    * feat: process nft sales for analytics
    
    * feat: sales-processor dockerized
    
    * chore: cleanup
    
    * chore: cronjob infra setup
    
    * chore: update package-lock.json
    
    * fix: update sales-processor to use new typeorm version
    
    * chore: fix shared/package.json

---
+++ infra/cronjobs/salesProcessor/eventbridge.ts
@@ -0,0 +1,45 @@
+import * as aws from '@pulumi/aws'
+
+import { getResourceName, getTags, isProduction } from '../../helper'
+
+const tags = {
+  cronjob: 'sales-processor',
+}
+
+const createEventBridgeRule = (): aws.cloudwatch.EventRule => {
+  const resourceName = getResourceName('salesProcessor-eventRule')
+  return new aws.cloudwatch.EventRule('salesProcessor-eventRule', {
+    name: resourceName,
+    scheduleExpression: isProduction() ? 'cron(00 04 * * ? *)' : 'cron(00 04 1 * ? *)',  // if prod, run daily at 04:00 UTC, otherwise 1x monthly
+    tags: getTags(tags),
+  })
+}
+
+export const createSalesProessorEventBridgeTarget = (
+  taskDef: aws.ecs.TaskDefinition,
+  subnets: string[],
+  cluster: aws.ecs.Cluster,
+): aws.cloudwatch.EventTarget => {
+  const rule = createEventBridgeRule()
+
+  return new aws.cloudwatch.EventTarget('salesProcessor-eventTarget', {
+    arn: cluster.arn,
+    ecsTarget: {
+      enableEcsManagedTags: true,
+      enableExecuteCommand: true,
+      launchType: 'FARGATE',
+      networkConfiguration: {
+        assignPublicIp: true,
+        subnets: subnets,
+      },
+      taskDefinitionArn: taskDef.arn,
+    },
+    retryPolicy: {
+      maximumEventAgeInSeconds: 600,
+      maximumRetryAttempts: 2,
+    },
+    // hardcoded iam role for eventbridge to trigger ecs
+    roleArn: 'arn:aws:iam::016437323894:role/service-role/Amazon_EventBridge_Invoke_ECS_306739191',
+    rule: rule.name,
+  })
+}
