a13e4564f5a9bdf247e98a15253797078d0ba403
@onmyown <davidwctan@gmail.com>
2022-10-11 14:48:36 -0500

    fix: add exception handler on seService (#591)
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>

--- packages/gql/src/service/searchEngine.service.ts
+++ packages/gql/src/service/searchEngine.service.ts
@@ -5,6 +5,7 @@ import { getRandomFloat } from '@nftcom/gql/helper/utils'
 import { core } from '@nftcom/gql/service'
 import { db, defs } from '@nftcom/shared'
 import { Collection as CollectionEntity, NFT as NFTEntity, Wallet as WalletEntity } from '@nftcom/shared/db/entity'
+import * as Sentry from '@sentry/node'
 
 const TYPESENSE_HOST = process.env.TYPESENSE_HOST
 const PROFILE_CONTRACT = process.env.TYPESENSE_HOST.startsWith('dev') ?
@@ -24,58 +25,63 @@ export class SearchEngineService {
   }
 
   indexNFTs = async (nfts: NFTEntity[]): Promise<boolean> => {
-    const nftsToIndex = await Promise.all(nfts.map(async (nft) => {
-      const ctx = {
-        chain: null,
-        network: null,
-        repositories: this._repos, // Only repositories is required for this query
-        user: null,
-        wallet: null,
-      }
-  
-      const collection = await core.resolveEntityById<NFTEntity, CollectionEntity>(
-        'contract',
-        defs.EntityType.NFT,
-        defs.EntityType.Collection,
-      )(nft, null, ctx)
-  
-      const wallet = await core.resolveEntityById<NFTEntity, WalletEntity>(
-        'walletId',
-        defs.EntityType.NFT,
-        defs.EntityType.Wallet,
-      )(nft, null, ctx)
-    
-      const tokenId = BigNumber.from(nft.tokenId).toString()
-      let traits = []
-      if (nft.metadata.traits.length < 100) {
-        traits = nft.metadata.traits.map((trait) => {
-          return {
-            type: trait.type,
-            value: `${trait.value}`,
-          }
-        })
-      }
-      return {
-        id: nft.id,
-        nftName: nft.metadata?.name || `#${tokenId}`,
-        nftType: nft.type,
-        tokenId,
-        traits,
-        imageURL: nft.metadata?.imageURL,
-        ownerAddr: wallet ? wallet.address : '',
-        chain: wallet ? wallet.chainName : '',
-        contractName: collection ? collection.name : '',
-        contractAddr: nft.contract || '',
-        marketplace: TYPESENSE_HOST.startsWith('prod') ? '' : 'OpenSea',
-        listingType: '',
-        listedPx: TYPESENSE_HOST.startsWith('prod') ? 0.0 : getRandomFloat(0.3, 2, 2),
-        currency: TYPESENSE_HOST.startsWith('prod') ? '' : 'ETH',
-        status: '',
-        isProfile: nft.contract === PROFILE_CONTRACT,
-      }
-    }))
-  
-    return this._client.insertDocuments('nfts', nftsToIndex)
+    try {
+      const nftsToIndex = await Promise.all(nfts.map(async (nft) => {
+        const ctx = {
+          chain: null,
+          network: null,
+          repositories: this._repos, // Only repositories is required for this query
+          user: null,
+          wallet: null,
+        }
+
+        const collection = await core.resolveEntityById<NFTEntity, CollectionEntity>(
+          'contract',
+          defs.EntityType.NFT,
+          defs.EntityType.Collection,
+        )(nft, null, ctx)
+
+        const wallet = await core.resolveEntityById<NFTEntity, WalletEntity>(
+          'walletId',
+          defs.EntityType.NFT,
+          defs.EntityType.Wallet,
+        )(nft, null, ctx)
+
+        const tokenId = BigNumber.from(nft.tokenId).toString()
+        let traits = []
+        if (nft.metadata.traits.length < 100) {
+          traits = nft.metadata.traits.map((trait) => {
+            return {
+              type: trait.type,
+              value: `${trait.value}`,
+            }
+          })
+        }
+        return {
+          id: nft.id,
+          nftName: nft.metadata?.name || `#${tokenId}`,
+          nftType: nft.type,
+          tokenId,
+          traits,
+          imageURL: nft.metadata?.imageURL,
+          ownerAddr: wallet ? wallet.address : '',
+          chain: wallet ? wallet.chainName : '',
+          contractName: collection ? collection.name : '',
+          contractAddr: nft.contract || '',
+          marketplace: TYPESENSE_HOST.startsWith('prod') ? '' : 'OpenSea',
+          listingType: '',
+          listedPx: TYPESENSE_HOST.startsWith('prod') ? 0.0 : getRandomFloat(0.3, 2, 2),
+          currency: TYPESENSE_HOST.startsWith('prod') ? '' : 'ETH',
+          status: '',
+          isProfile: nft.contract === PROFILE_CONTRACT,
+        }
+      }))
+
+      return this._client.insertDocuments('nfts', nftsToIndex)
+    } catch (err) {
+      Sentry.captureMessage(`Error in indexNFTs: ${err}`)
+      throw err
+    }
   }
 
   deleteNFT = (nftId: string): Promise<boolean> => {
@@ -88,38 +94,48 @@ export class SearchEngineService {
     return officialVal + nftcomVal
   }
   indexCollections = async (collections: CollectionEntity[]): Promise<boolean> => {
-    const collectionsToIndex = await Promise.all(
-      collections
-        .filter((collection) => !collection.isSpam)
-        .map(async (collection) => {
-          const nft = await this._repos.nft.findOne({
-            select: ['type'],
-            where: {
-              contract: utils.getAddress(collection.contract),
-              chainId: collection.chainId,
-            },
-          })
-      
-          return {
-            id: collection.id,
-            contractAddr: collection.contract,
-            contractName: collection.name,
-            chain: collection.chainId,
-            description: collection.description || '',
-            floor: 0.0,
-            nftType: nft ? nft.type : '',
-            score: this._calculateCollectionScore(collection),
-          }
-        }),
-    )
-    
-    return this._client.insertDocuments('collections', collectionsToIndex)
+    try {
+      const collectionsToIndex = await Promise.all(
+        collections
+          .filter((collection) => !collection.isSpam)
+          .map(async (collection) => {
+            const nft = await this._repos.nft.findOne({
+              select: ['type'],
+              where: {
+                contract: utils.getAddress(collection.contract),
+                chainId: collection.chainId,
+              },
+            })
+
+            return {
+              id: collection.id,
+              contractAddr: collection.contract,
+              contractName: collection.name,
+              chain: collection.chainId,
+              description: collection.description || '',
+              floor: 0.0,
+              nftType: nft ? nft.type : '',
+              score: this._calculateCollectionScore(collection),
+            }
+          }),
+      )
+
+      return this._client.insertDocuments('collections', collectionsToIndex)
+    } catch (err) {
+      Sentry.captureMessage(`Error in indexCollections: ${err}`)
+      throw err
+    }
   }
 
   deleteCollections = async (collections: CollectionEntity[]): Promise<void> => {
-    await Promise.all(collections.map(async (collection) => {
-      await this._client.removeDocument('collections', collection.id)
-    }))
+    try {
+      await Promise.all(collections.map(async (collection) => {
+        await this._client.removeDocument('collections', collection.id)
+      }))
+    } catch (err) {
+      Sentry.captureMessage(`Error in deleteCollections: ${err}`)
+      throw err
+    }
   }
 
 }
