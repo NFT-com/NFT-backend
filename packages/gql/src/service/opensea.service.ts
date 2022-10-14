00bcced03ad1d20c630f9c42755023a912edd772
deusexmachinadev <67821526+deusexmachinadev@users.noreply.github.com>
2022-10-14 23:17:34 +0530

    feat: logs for listing creation (#598)

--- packages/gql/src/service/opensea.service.ts
+++ packages/gql/src/service/opensea.service.ts
@@ -454,6 +454,8 @@ export const createSeaportListing = async (
     return null
   } catch (err) {
     logger.error(`Error in createSeaportListing: ${err}`)
+    logger.log('seaport signature', signature)
+    logger.log('createSeaportListing payload', parameters)
     // Sentry.captureMessage(`Error in createSeaportListing: ${err}`)
     throw err
   }
