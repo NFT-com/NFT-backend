ed281b0de0c6bbf04ffdcf1d08f983706ef9ef0a
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-10-19 03:33:51 -0400

    feat: nullable true for userId and walletId

--- packages/gql/src/service/index.ts
+++ packages/gql/src/service/index.ts
@@ -1,3 +1,4 @@
+export * as alchemyService from './alchemy.service'
 export * as contractData from './contractData.service'
 export * as core from './core.service'
 export * as nftService from './nft.service'
