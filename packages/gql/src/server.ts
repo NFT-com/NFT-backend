bdb9f4e5c6fce7e3d5b0cab91124067e61973033
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-11-07 08:34:30 -0600

    fix: compare

--- packages/gql/src/server.ts
+++ packages/gql/src/server.ts
@@ -163,7 +163,7 @@ export const start = async (): Promise<void> => {
 
     if (foundUser?.isEmailConfirmed) {
       return res.status(400).json({ message: 'User already exists' })
-    } else if (foundUser?.confirmEmailTokenExpiresAt > new Date()) {
+    } else if (Number(new Date(foundUser?.confirmEmailTokenExpiresAt)) > new Date()) {
       return res.status(400).json({ message: 'User already exists' })
     } else {
       return repositories.user.save({
