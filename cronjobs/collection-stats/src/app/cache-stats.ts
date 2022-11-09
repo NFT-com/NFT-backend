d36d7da644e73e606d21d03d0a1ffc454869ac7f
t-morgan <tim@immutableholdings.com>
2022-11-09 10:29:38 -0500

    feat: update collection leaderboard in collection-stats cronjob

--- cronjobs/collection-stats/src/app/cache-stats.ts
+++ cronjobs/collection-stats/src/app/cache-stats.ts
@@ -1,10 +1,13 @@
 import { updateContractStats } from '@nftcom/contract-data'
+import { updateCollectionLeaderboard } from '@nftcom/leaderboard'
 import { db } from '@nftcom/shared'
 
 import { getConnection } from './data-source'
 
 export const cacheStats = async (): Promise<void> => {
   await getConnection()
-  const collections =  await db.newRepositories().collection.findAllOfficial()
+  const collectionRepo = db.newRepositories().collection
+  const collections =  await collectionRepo.findAllOfficial()
   await updateContractStats(collections)
+  await updateCollectionLeaderboard(collectionRepo)
 }
--- packages/gql/src/test/resolver/collection.resolver.unit.spec.ts
+++ packages/gql/src/test/resolver/collection.resolver.unit.spec.ts
@@ -2,7 +2,7 @@ import { Context } from '@nftcom/gql/defs'
 import { default as collectionResolver } from '@nftcom/gql/resolver/collection.resolver'
 import { fetchData } from '@nftcom/nftport-client'
 import { Repository } from '@nftcom/shared/db/db'
-import { NFTRepository } from '@nftcom/shared/db/repository'
+import { CollectionRepository } from '@nftcom/shared/db/repository'
 
 let mockCacheData = {}
 jest.mock('@nftcom/cache', () => ({
@@ -53,7 +53,7 @@ describe('collection resolver', () => {
             { count: 99, type: 'Trait B', value: 'x' },
             { count: 97, type: 'Trait B', value: 'y' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       const traitSummary = await collectionResolver.Query.collectionTraits(undefined, { input: { contract: '0x00000' } }, mockCtx)
@@ -74,197 +74,6 @@ describe('collection resolver', () => {
     beforeEach(() => {
       mockCacheData = {}
       jest.resetAllMocks()
-    })
-    it('should sort collections with highest ranked leaders at the top', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000' },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002' },
-            { id: '3', contract: '0x0003' },
-            { id: '4', contract: '0x0004' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 0 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 2 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 3 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 4 } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-
-      expect(leaderboard.items).toEqual([
-        { id: '4', contract: '0x0004', stats: { seven_day_sales: 4 } },
-        { id: '3', contract: '0x0003', stats: { seven_day_sales: 3 } },
-        { id: '2', contract: '0x0002', stats: { seven_day_sales: 2 } },
-        { id: '1', contract: '0x0001', stats: { seven_day_sales: 1 } },
-        { id: '0', contract: '0x0000', stats: { seven_day_sales: 0 } },
-      ])
-      expect(leaderboard.totalItems).toEqual(5)
-    })
-
-    it('should sort collections when a collection does not have stats', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000' },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002' },
-            { id: '3', contract: '0x0003' },
-            { id: '4', contract: '0x0004' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 0 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1 } })
-        .mockResolvedValueOnce({ })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 3 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 4 } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-
-      expect(leaderboard.items).toEqual([
-        { id: '4', contract: '0x0004', stats: { seven_day_sales: 4 } },
-        { id: '3', contract: '0x0003', stats: { seven_day_sales: 3 } },
-        { id: '1', contract: '0x0001', stats: { seven_day_sales: 1 } },
-        { id: '0', contract: '0x0000', stats: { seven_day_sales: 0 } },
-        { id: '2', contract: '0x0002' },
-      ])
-      expect(leaderboard.totalItems).toEqual(5)
-    })
-
-    it('should sort collections falling back to persisted stats when needed', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000', totalVolume: 90 },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002', totalVolume: 100 },
-            { id: '3', contract: '0x0003' },
-            { id: '4', contract: '0x0004' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockImplementationOnce(() => { throw new Error() })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1 } })
-        .mockImplementationOnce(() => { throw new Error() })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 3 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 4 } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-      const leaderboardItems = leaderboard.items.map(({ totalVolume, ...rest }) => rest)
-
-      expect(leaderboardItems).toEqual([
-        { id: '4', contract: '0x0004', stats: { seven_day_sales: 4 } },
-        { id: '3', contract: '0x0003', stats: { seven_day_sales: 3 } },
-        { id: '1', contract: '0x0001', stats: { seven_day_sales: 1 } },
-        { id: '2', contract: '0x0002' },
-        { id: '0', contract: '0x0000' },
-      ])
-      expect(leaderboard.totalItems).toEqual(5)
-    })
-
-    it('should sort collections with different stats available', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000' },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002' },
-            { id: '3', contract: '0x0003' },
-            { id: '4', contract: '0x0004' },
-            { id: '5', contract: '0x0005' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: Number.MAX_SAFE_INTEGER } })
-        .mockResolvedValueOnce({ statistics: { seven_day_volume: 1 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_volume: Number.MAX_SAFE_INTEGER } })
-        .mockResolvedValueOnce({ statistics: { total_volume: 1 } })
-        .mockResolvedValueOnce({ statistics: { total_volume: Number.MAX_SAFE_INTEGER } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-
-      expect(leaderboard.items).toEqual([
-        { id: '1', contract: '0x0001', stats: { seven_day_sales: Number.MAX_SAFE_INTEGER } },
-        { id: '0', contract: '0x0000', stats: { seven_day_sales: 1 } },
-        { id: '3', contract: '0x0003', stats: { seven_day_volume: Number.MAX_SAFE_INTEGER } },
-        { id: '2', contract: '0x0002', stats: { seven_day_volume: 1 } },
-        { id: '5', contract: '0x0005', stats: { total_volume: Number.MAX_SAFE_INTEGER } },
-        { id: '4', contract: '0x0004', stats: { total_volume: 1 } },
-      ])
-      expect(leaderboard.totalItems).toEqual(6)
-    })
-
-    it('should sort collections with all stats available', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000' },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1, seven_day_volume: 1, total_volume: 1 } })
-        .mockResolvedValueOnce({ statistics: {
-          seven_day_sales: Number.MAX_SAFE_INTEGER, seven_day_volume: 1, total_volume: 1,
-        } })
-        .mockResolvedValueOnce({ statistics: {
-          seven_day_sales: 1, seven_day_volume: Number.MAX_SAFE_INTEGER, total_volume: Number.MAX_SAFE_INTEGER,
-        } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-
-      expect(leaderboard.items).toEqual([
-        { id: '1', contract: '0x0001', stats: {
-          seven_day_sales: Number.MAX_SAFE_INTEGER, seven_day_volume: 1, total_volume: 1,
-        } },
-        { id: '2', contract: '0x0002', stats: {
-          seven_day_sales: 1, seven_day_volume: Number.MAX_SAFE_INTEGER, total_volume: Number.MAX_SAFE_INTEGER,
-        } },
-        { id: '0', contract: '0x0000', stats: { seven_day_sales: 1, seven_day_volume: 1, total_volume: 1 } },
-      ])
-      expect(leaderboard.totalItems).toEqual(3)
-    })
-
-    fit('should sort collections when stats near', async () => {
-      mockCtx.repositories = {
-        collection: {
-          findAllOfficial: jest.fn().mockResolvedValue([
-            { id: '0', contract: '0x0000' },
-            { id: '1', contract: '0x0001' },
-            { id: '2', contract: '0x0002' },
-          ]),
-        }  as unknown as NFTRepository,
-      } as unknown as Repository
-
-      mockFetchData
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 1, seven_day_volume: 2, total_volume: 3 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 3, seven_day_volume: 2, total_volume: 1 } })
-        .mockResolvedValueOnce({ statistics: { seven_day_sales: 2, seven_day_volume: 3, total_volume: 2 } })
-
-      const leaderboard = await collectionResolver.Query.collectionLeaderboard(undefined, {}, mockCtx)
-
-      expect(leaderboard.items).toEqual([
-        { id: '1', contract: '0x0001', stats: { seven_day_sales: 3, seven_day_volume: 2, total_volume: 1 } },
-        { id: '2', contract: '0x0002', stats: { seven_day_sales: 2, seven_day_volume: 3, total_volume: 2 } },
-        { id: '0', contract: '0x0000', stats: { seven_day_sales: 1, seven_day_volume: 2, total_volume: 3 } },
-      ])
-      expect(leaderboard.totalItems).toEqual(3)
     })
 
     it('should paginate the top 20 by default', async () => {
@@ -293,7 +102,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -359,7 +168,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -408,7 +217,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -466,7 +275,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -519,7 +328,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -571,7 +380,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
@@ -626,7 +435,7 @@ describe('collection resolver', () => {
             { id: '19', contract: '0x0019' },
             { id: '20', contract: '0x0020' },
           ]),
-        }  as unknown as NFTRepository,
+        }  as unknown as CollectionRepository,
       } as unknown as Repository
 
       let sales = 0
