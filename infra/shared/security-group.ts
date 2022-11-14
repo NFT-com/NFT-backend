86d7cfa777e282761c1d23967969c009cac02348
Joey <joey@nft.com>
2022-11-14 10:21:06 -0800

    fix: https cert (#669)
    * fix: https cert
    
    * fix: update redis SG inbound
    
    Co-authored-by: Joey Punzel <joey@immutableholdings.com>

--- infra/shared/security-group.ts
+++ infra/shared/security-group.ts
@@ -119,6 +119,8 @@ export const createSecurityGroups = (config: pulumi.Config, vpc: ec2.Vpc): SGOut
           buildIngressRule(6379, 'tcp', [web.id]),
           buildIngressRule(6379, 'tcp', [webEcs.id]),
           buildIngressRule(6379, 'tcp', [internalEcs.id]),
+          buildIngressRule(5432, 'tcp', [pulumi.output('sg-0bad265e467cdec96')]), // Bastion Host
+          buildIngressRule(5432, 'tcp', [pulumi.output('sg-0bd5dceea498f0356')]), // Prod Stream ECS Cluster 
         ]
         : [buildIngressRule(6379)]
     ,
