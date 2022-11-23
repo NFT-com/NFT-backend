74b1442b26f08213aac5bc384483b274187d7e55
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-11-23 19:53:31 -0400

    fix: try 0 cache

--- packages/gql/src/resolver/nft.resolver.ts
+++ packages/gql/src/resolver/nft.resolver.ts
@@ -273,7 +273,7 @@ const returnProfileNFTs = async (
           }
         }
       }
-      await cache.set(cacheKey, JSON.stringify(nfts), 'EX', 30) // 30 second cache
+      // await cache.set(cacheKey, JSON.stringify(nfts), 'EX', 30) // 30 second cache
     }
 
     let paginatedNFTs: Array<gql.NFT>
