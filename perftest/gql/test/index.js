dd5ccc2bbb7200e75d6775893f366af212991ba5
t-morgan <tim@immutableholdings.com>
2022-11-28 10:40:49 -0500

    fix: get valid auth token for perf tests

--- perftest/gql/test/index.js
+++ perftest/gql/test/index.js
@@ -1,8 +1,4 @@
 import http from 'k6/http'
-import { addDays } from 'date-fns'
-import { Wallet } from 'ethers'
-
-import { helper } from '@nftcom/shared'
 
 const queryData = JSON.parse(open(`./${__ENV.QUERY_DIR}/queries.json`))
 
@@ -16,35 +12,21 @@ export const options = {
   },
 }
 
-const getAuth = async () => {
-  const authMessage = 'I\'d like to sign in'
-  const privateKey = 'a2f890d2f7023d5eeba7f5c600bd50650ca59bd7e7007af8e016cd7abdc9af5d'
-  const signer = new Wallet(privateKey)
-  if (!signer) {
-    return Promise.reject(new Error('invalid private key'))
-  }
-  const timestamp = addDays(helper.toUTCDate(), 7)
-  const unixTimestamp = Math.floor(timestamp.getTime() / 1000 )
-  const signature = await signer.signMessage(`${authMessage} ${unixTimestamp}`)
-  return [signature, unixTimestamp]
-}
-
-const getRequests = async () => {
- const requests = []
- for (const data of queryData) {
-  const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
+const requests = []
+const [timestamp, signature] = __ENV.GQL_AUTH.split(' ')
+const url = `https://${__ENV.GQL_HOSTNAME}/graphql/`
+for (const data of queryData) {
   const payload = JSON.stringify({
     query: data.query,
     variables: data.variables,
   })
-  const [signature, timestamp] = await getAuth()
   const params = {
     headers: {
       'Content-Type': 'application/json',
       'network': 'ethereum',
       'chain-id': '5',
       'authorization': signature,
-      timestamp
+      timestamp,
     },
     tags: {
       rw: data.operation === 'Mutation' ? 'write' : 'read'
@@ -52,19 +34,14 @@ const getRequests = async () => {
   }
 
   requests.push(['POST', url, payload, params])
-  }
-  return requests
 }
 
-
 export default function () {
-  getRequests().then((requests) => {
-    const responses = http.batch(requests)
+  const responses = http.batch(requests)
 
-    for (const response of responses) {
-      if (response.status >= 400) {
-        console.error(`Received HTTP ${response.status} for ${response.url}, body: ${response.body}`);
-      }
+  for (const response of responses) {
+    if (response.status >= 400) {
+      console.error(`Received HTTP ${response.status} for ${response.url}, body: ${response.body}`);
     }
-  })
+  }
 }
