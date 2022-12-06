481a65182f613d88fa35dce7db18ca1633b37ef8
t-morgan <tim@immutableholdings.com>
2022-12-06 08:28:00 -0500

    feat: sort collection leaderboard by volume

--- packages/leaderboard/src/lib/collection-leaderboard.ts
+++ packages/leaderboard/src/lib/collection-leaderboard.ts
@@ -1,5 +1,3 @@
-import { DataFrame, MinMaxScaler, Series } from 'danfojs-node'
-
 import { cache } from '@nftcom/cache'
 import { fetchData } from '@nftcom/nftport-client'
 import { entity, repository } from '@nftcom/shared'
@@ -36,30 +34,6 @@ const createDefaultStats = (stats: any, totalVolume: number): any => {
   }
 }
 
-/**
- * Calculates a score for three columns of a DataFrame
- * 
- * The goal is to give the highest weight to the first column,
- * the second highest weight to the second column,
- * and the lowest weight to the third column.
- * 
- * For example, calculateScore(df_enc, ['seven_day_sales', 'seven_day_volume', 'total_volume'])
- * will score 'seven_day_sales' as the most important factor,
- * 'seven_day_volume' as the second most important, and 'total_volume' as the least important.
- * 
- * @param df The DataFrame containing the data
- * @param columns Three DataFrame columns to use
- * @returns {Series} danfo.js Series containing scores indexed by collection contract
- */
-const calculateScore = (df: DataFrame, columns: string[]): Series => {
-  return df.loc({ columns }).apply((data: number[]) => {
-    // Get coefficients so score ranges do not overlap
-    const c0 = data[0] ? 9 : 0
-    const c1 = data[1] ? 2 : 0
-    return (c0 * Math.pow(1 + data[0], 3)) + (c1 * Math.pow(1 + data[1], 2)) + data[2]
-  }) as Series
-}
-
 const fetchCollections = async (collectionRepo: repository.CollectionRepository):
 Promise<(entity.Collection & {stats?: any})[]> => {
   const collections: (entity.Collection & {stats?: any})[] = await collectionRepo.findAllOfficial()
@@ -68,7 +42,7 @@ Promise<(entity.Collection & {stats?: any})[]> => {
       const { statistics: stats } = await fetchData('stats', [collection.contract])
       collection.stats = stats
     } catch (_e) {
-      // noop
+      collection.stats = createDefaultStats({}, collection.totalVolume)
     }
   }
   return collections
@@ -94,31 +68,17 @@ export const updateCollectionLeaderboard = async (collectionRepo: repository.Col
 Promise<(entity.Collection & {stats?: any})[]> => {
   // Get official collections and add NFTPort stats
   const collections: (entity.Collection & {stats?: any})[] = await fetchCollections(collectionRepo)
-  // Create a dataframe with just the stats and contract address
-  const data = collections.map((c) => {
-    const { updated_date, ...stats } = createDefaultStats(c.stats, c.totalVolume)
-    return { ...stats, contract: c.contract }
-  })
-  const df = new DataFrame(data)
-  df.setIndex({ column: 'contract', drop: true, inplace: true })
-  // Normalize stats for scoring
-  const df_enc = new MinMaxScaler().fit(df).transform(df).fillNa(0)
-  // Get scores for each timeframe
-  const scores_24h = calculateScore(df_enc, ['one_day_sales', 'one_day_volume', 'total_volume'])
-  const scores_7d = calculateScore(df_enc, ['seven_day_sales', 'seven_day_volume', 'total_volume'])
-  const scores_30d = calculateScore(df_enc, ['thirty_day_sales', 'thirty_day_volume', 'total_volume'])
-  const scores_all = calculateScore(df_enc, ['total_sales', 'total_volume', 'floor_price'])
   // Save score to cache
-  for (const [index, contract] of df_enc.index.entries()) {
-    const score_24h = scores_24h.values[index].toString()
-    const score_7d = scores_7d.values[index].toString()
-    const score_30d = scores_30d.values[index].toString()
-    const score_all = scores_all.values[index].toString()
+  for (const collection of collections) {
+    const score_24h = collection.stats.one_day_volume.toString()
+    const score_7d = collection.stats.seven_day_volume.toString()
+    const score_30d = collection.stats.thirty_day_volume.toString()
+    const score_all = collection.stats.total_volume.toString()
     await Promise.all([
-      cache.zadd('COLLECTION_LEADERBOARD_24h', score_24h, contract),
-      cache.zadd('COLLECTION_LEADERBOARD_7d', score_7d, contract),
-      cache.zadd('COLLECTION_LEADERBOARD_30d', score_30d, contract),
-      cache.zadd('COLLECTION_LEADERBOARD_all', score_all, contract),
+      cache.zadd('COLLECTION_LEADERBOARD_24h', score_24h, collection.contract),
+      cache.zadd('COLLECTION_LEADERBOARD_7d', score_7d, collection.contract),
+      cache.zadd('COLLECTION_LEADERBOARD_30d', score_30d, collection.contract),
+      cache.zadd('COLLECTION_LEADERBOARD_all', score_all, collection.contract),
     ])
   }
   // Get requested leaderboard back from cache
