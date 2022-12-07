5f7587b7fee15f71630bb32f63ace486603aa633
t-morgan <tim@immutableholdings.com>
2022-12-07 11:17:17 -0500

    fix: remove unneeded traces

--- packages/gql/src/tracer.ts
+++ packages/gql/src/tracer.ts
@@ -3,14 +3,14 @@ import opentelemetry = require('@opentelemetry/api');
 import { Attributes, SpanKind } from '@opentelemetry/api'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express'
+import { ExpressInstrumentation, ExpressLayerType } from '@opentelemetry/instrumentation-express'
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql'
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis'
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg'
 import { Resource } from '@opentelemetry/resources'
 import { AlwaysOnSampler, Sampler, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { ConsoleSpanExporter, NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { SemanticAttributes, SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 
 type FilterFunction = (spanName: string, spanKind: SpanKind, attributes: Attributes) => boolean;
@@ -31,7 +31,10 @@ function filterSampler(filterFn: FilterFunction, parent: Sampler): Sampler {
 
 function ignoreSpan(_spanName: string, spanKind: SpanKind, attributes: Attributes): boolean {
   return attributes[SemanticAttributes.HTTP_METHOD] === 'OPTIONS'
-    || attributes[SemanticAttributes.HTTP_TARGET] === '/.well-known/apollo/server-health'
+    || (attributes[SemanticAttributes.HTTP_TARGET]
+        && ['/.well-known/apollo/server-health', '/favicon.ico'].includes(attributes[SemanticAttributes.HTTP_TARGET].toString()))
+        || (attributes[SemanticAttributes.HTTP_ROUTE]
+          && ['/.well-known/apollo/server-health', '/'].includes(attributes[SemanticAttributes.HTTP_ROUTE].toString()))
     || (attributes[SemanticAttributes.HTTP_URL]
         && attributes[SemanticAttributes.HTTP_URL].toString().includes('sentry.io'))
 }
@@ -48,18 +51,23 @@ export const setupTracing = (serviceName: string): opentelemetry.Tracer => {
     instrumentations: [
       // Express instrumentation expects HTTP layer to be instrumented
       new HttpInstrumentation(),
-      new ExpressInstrumentation(),
+      new ExpressInstrumentation({
+        ignoreLayersType: [ExpressLayerType.MIDDLEWARE],
+      }),
       new GraphQLInstrumentation(),
       new IORedisInstrumentation({
         requireParentSpan: false,
       }),
-      new PgInstrumentation(),
+      new PgInstrumentation({
+        requireParentSpan: true,
+      }),
     ],
   })
 
   const exporter = new OTLPTraceExporter()
 
   provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
 
   // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
   provider.register()
