b8eef415f810adf7d82a1a269d5e5dce0aa803ba
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-12-20 08:28:48 -0600

    fix: add feature flag

--- packages/gql/src/service/hedera.service.ts
+++ packages/gql/src/service/hedera.service.ts
@@ -1,13 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Client, TopicMessageQuery, TopicMessageSubmitTransaction } from '@hashgraph/sdk'
 import SubscriptionHandle from '@hashgraph/sdk/lib/topic/SubscriptionHandle'
 import { _logger } from '@nftcom/shared'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { isProduction } from '../config'
 import { IHederaConsensusService } from '../defs/hedera'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const logger = _logger.Factory(_logger.Context.General, _logger.Context.GraphQL)
 
 class _HederaConsensusService implements IHederaConsensusService {
@@ -18,44 +15,58 @@ class _HederaConsensusService implements IHederaConsensusService {
   mirrorNetwork: string
 
   constructor() {
-    // if (isProduction()) {
-    //   this.client = Client.forMainnet()
-    //   this.mirrorNetwork = 'mainnet-public.mirrornode.hedera.com:443'
-    // } else {
-    //   this.client = Client.forTestnet()
-    //   this.mirrorNetwork = 'hcs.testnet.mirrornode.hedera.com:5600'
-    // }
+    if (!process.env.HCS_ENABLED) {
+      return
+    }
 
-    // this.topicId = process.env.HCS_TOPIC_ID
+    if (isProduction()) {
+      this.client = Client.forMainnet()
+      this.mirrorNetwork = 'mainnet-public.mirrornode.hedera.com:443'
+    } else {
+      this.client = Client.forTestnet()
+      this.mirrorNetwork = 'hcs.testnet.mirrornode.hedera.com:5600'
+    }
+
+    this.topicId = process.env.HCS_TOPIC_ID
   }
 
   subscribe(): void {
+    if (!process.env.HCS_ENABLED) {
+      return
+    }
+
     // build consensus client
     this.client.setMirrorNetwork(this.mirrorNetwork)
   
     // subscribe to topic to listen to all new messages submitted to it
-    // this.HCS = new TopicMessageQuery()
-    //   .setTopicId(this.topicId)
-    //   .subscribe(this.client,
-    //     error => { logger.error( error ) },
-    //     response => {
-    //       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    //       const message = response.contents.toString()
-    //       // logger.info(message)
-    //     })
+    this.HCS = new TopicMessageQuery()
+      .setTopicId(this.topicId)
+      .subscribe(this.client,
+        error => { logger.error( error ) },
+        response => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const message = response.contents.toString()
+          // logger.info(message)
+        })
   }
 
   unsubscribe(): void {
-    return
+    if (!process.env.HCS_ENABLED) {
+      return
+    }
 
-    // if (!this.HCS) {
-    //   return
-    // }
+    if (!this.HCS) {
+      return
+    }
 
-    // this.HCS.unsubscribe()
+    this.HCS.unsubscribe()
   }
 
   async submitMessage(_message: string): Promise<void> {
+    if (!process.env.HCS_ENABLED) {
+      return
+    }
+    
     // build client
     const accountId = process.env.HCS_ACCOUNT_ID
     const privateKey = process.env.HCS_PRIVATE_KEY
@@ -63,10 +74,10 @@ class _HederaConsensusService implements IHederaConsensusService {
     this.client.setOperator(accountId, privateKey)
 
     // specify topic id and submit a new message to HCS
-    // await new TopicMessageSubmitTransaction()
-    //   .setTopicId(this.topicId)
-    //   .setMessage(_message)
-    //   .execute(this.client)
+    await new TopicMessageSubmitTransaction()
+      .setTopicId(this.topicId)
+      .setMessage(_message)
+      .execute(this.client)
   }
 
 }
