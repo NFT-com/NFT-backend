2606c20104c25c4af651ae05544307be2656f752
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-01-03 13:44:37 -0500

    feat: add created at option to activites query (#753)

--- packages/shared/src/db/utils.ts
+++ packages/shared/src/db/utils.ts
@@ -1,5 +1,5 @@
 import { newRepositories } from './db'
-import { NFT, TxOrder } from './entity'
+import { NFT, TxActivity, TxOrder } from './entity'
 
 const repositories = newRepositories()
 
@@ -8,6 +8,26 @@ export const getNFTsFromTxOrders = async (orders: TxOrder[]): Promise<NFT[]> => 
   const nftsSeen = {}
   for (const order of orders) {
     for (const nftId of order.activity.nftId) {
+      if (!nftsSeen[nftId]) {
+        const idParts = nftId.split('/')
+        nfts.push(await repositories.nft.findOne({
+          where: {
+            contract: idParts[1],
+            tokenId: idParts[2],
+          },
+        }))
+        nftsSeen[nftId] = true
+      }
+    }
+  }
+  return nfts
+}
+
+export const getNFTsFromTxActivities = async (activities: TxActivity[]): Promise<NFT[]> => {
+  const nfts = []
+  const nftsSeen = {}
+  for (const activity of activities) {
+    for (const nftId of activity.nftId) {
       if (!nftsSeen[nftId]) {
         const idParts = nftId.split('/')
         nfts.push(await repositories.nft.findOne({
