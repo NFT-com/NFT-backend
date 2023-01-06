8114eeaa3d7e2c5aba582e832795df4ebdaaf06f
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-01-06 09:31:10 -0500

    fix: cannot use concurrently in a transaction (#764)

--- packages/shared/src/db/migration/1673014408233-txActivityNftIdExpirationIndexes.ts
+++ packages/shared/src/db/migration/1673014408233-txActivityNftIdExpirationIndexes.ts
@@ -5,8 +5,8 @@ export class txActivityNftIdExpirationIndexes1673014408233 implements MigrationI
   name = 'txActivityNftIdExpirationIndexes1673014408233'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE INDEX CONCURRENTLY "IDX_30515d2ad413e74246306659d3" ON "tx_activity" ("expiration") ')
-    await queryRunner.query('CREATE INDEX CONCURRENTLY idx_tx_activity_nftid ON "tx_activity" USING GIN ("nftId") ')
+    await queryRunner.query('CREATE INDEX "IDX_30515d2ad413e74246306659d3" ON "tx_activity" ("expiration") ')
+    await queryRunner.query('CREATE INDEX "idx_tx_activity_nftid" ON "tx_activity" USING GIN ("nftId") ')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
