b313825907c28539d426a1ed8a6a3f6e5c897613
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-01-06 09:26:47 -0500

    feat: created indexes to alleviate prod CPU spikes (#763)

---
+++ packages/shared/src/db/migration/1673014408233-txActivityNftIdExpirationIndexes.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class txActivityNftIdExpirationIndexes1673014408233 implements MigrationInterface {
+
+  name = 'txActivityNftIdExpirationIndexes1673014408233'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE INDEX CONCURRENTLY "IDX_30515d2ad413e74246306659d3" ON "tx_activity" ("expiration") ')
+    await queryRunner.query('CREATE INDEX CONCURRENTLY idx_tx_activity_nftid ON "tx_activity" USING GIN ("nftId") ')
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP INDEX "public"."IDX_30515d2ad413e74246306659d3"')
+    await queryRunner.query('DROP INDEX "public"."idx_tx_activity_nftid"')
+  }
+
+}
