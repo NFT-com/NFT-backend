c047fcf67b52a55d10a2e2b978dc8da5cdf5beda
t-morgan <tim@immutableholdings.com>
2023-01-19 12:35:57 -0500

    chore: drop max score for largest collections

--- packages/gql/src/service/searchEngine.service.ts
+++ packages/gql/src/service/searchEngine.service.ts
@@ -53,7 +53,7 @@ const getListingCurrencyAddress = (listing: TxActivityDAO): string => {
   }
 }
 
-const LARGEST_COLLECTIONS = defs.LARGE_COLLECTIONS.slice(0, 2)
+const LARGEST_COLLECTIONS = defs.LARGE_COLLECTIONS.slice(0, 3)
 export const SearchEngineService = (client = SearchEngineClient.create(), repos: any = db.newRepositories()): any => {
   const _calculateNFTScore = (collection: entity.Collection, hasListings: boolean): number => {
     const curatedVal = collection?.isCurated ? 1 : 0
@@ -62,7 +62,6 @@ export const SearchEngineService = (client = SearchEngineClient.create(), repos:
     const score = curatedVal + officialVal + listingsVal
     if (score === 3) {
       const multiplier = LARGEST_COLLECTIONS.includes(collection.contract)
-        && Math.floor(Math.random() * 10) % 10 !== 0
         ? 9_000_000
         : 10_000_000
       return score + Math.floor(Math.random() * multiplier)
