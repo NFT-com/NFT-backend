e7d8501911acf06d0fb0407773f0fc4c67f64e8a
t-morgan <tim@immutableholdings.com>
2023-01-19 09:53:38 -0500

    chore: separate unit and integration tests

--- packages/gql/jest.integration.config.ts
+++ packages/gql/jest.integration.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'gql',
+  displayName: 'gql-integration',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ export default {
     },
   },
   testEnvironment: 'node',
-  testRegex: '^.+\\.unit\\.spec\\.[tj]s$',
+  testRegex: '^.+\\.int\\.spec\\.[tj]s$',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
