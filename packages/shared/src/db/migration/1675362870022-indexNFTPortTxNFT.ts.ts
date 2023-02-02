4b7243595dd7b1f61495d060c7a7f2144b3a0d86
deusexmachinadev <67821526+deusexmachinadev@users.noreply.github.com>
2023-02-03 03:01:21 +0530

    Feat/nftport index (#828)
    * fix: update profile.resolver.ts (#672)
    
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>
    
    * Fix: dynamic auth header ( One-time nonce ) : Do not merge until FE integrate new format. (#654)
    
    * fix: update user entity to include nonce
    
    * fix: update createContext function to check nonce for one-time authentication
    
    * fix: update createContext function to check nonce for one-time authentication
    
    * fix: remove nonce and integrate DateTime for nonce and 7 days expiration
    
    * fix: merge changes from main
    
    * fix: add auth expiration date on doppler and check
    
    * fix: add some comment
    
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>
    
    * fix: index on NFTPort Tx Table
    
    * feat: index on nft
    
    * feat: index on nft
    
    * fix: jsonb fix
    
    * fix: jsonb fix
    
    ---------
    
    Co-authored-by: @onmyown <davidwctan@gmail.com>
    Co-authored-by: Jason Liu <jasonliudan@gmail.com>

--- packages/shared/src/db/migration/1675362870022-indexNFTPortTxNFT.ts.ts
+++ packages/shared/src/db/migration/1675362870022-indexNFTPortTxNFT.ts.ts
@@ -5,13 +5,13 @@ export class indexNFTPortTxNFT1675360976823 implements MigrationInterface {
   name = 'indexNFTPortTxNFT1675360976823'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE nft_port_transaction ALTER COLUMN nft SET DATA TYPE jsonb USING nft::jsonb')
+    await queryRunner.query('ALTER TABLE "nft_port_transaction" ALTER COLUMN "nft" SET DATA TYPE jsonb USING "nft"::jsonb')
     await queryRunner.query('CREATE INDEX "idx_nft_port_transaction_nft" ON "nft_port_transaction" USING GIN ("nft") ')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('DROP INDEX "public"."idx_nft_port_transaction_nft"')
-    await queryRunner.query('ALTER TABLE nft_port_transaction ALTER COLUMN nft SET DATA TYPE json USING nft::json')
+    await queryRunner.query('ALTER TABLE "nft_port_transaction" ALTER COLUMN "nft" SET DATA TYPE json USING "nft"::json')
   }
 
 }
