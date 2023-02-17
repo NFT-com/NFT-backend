ed2839ab3bf7eabf8b5297e8c8be787cc98dd006
Jason Liu <jasonliudan@gmail.com>
2023-02-17 13:37:13 -0800

    fix: update txTransaction repository

--- packages/shared/src/db/repository/txTransaction.repository.ts
+++ packages/shared/src/db/repository/txTransaction.repository.ts
@@ -1,4 +1,7 @@
+import { SelectQueryBuilder } from 'typeorm'
+
 import { TxTransaction } from '@nftcom/shared/db/entity'
+import { ActivityType } from '@nftcom/shared/defs'
 
 import { BaseRepository } from './base.repository'
 
@@ -6,6 +9,20 @@ export class TxTransactionRepository extends BaseRepository<TxTransaction> {
 
   constructor() {
     super(TxTransaction)
+  }
+
+  public findSaleActivities = (
+    address: string,
+  ): Promise<TxTransaction[]> => {
+    const queryBuilder: SelectQueryBuilder<TxTransaction> = this.getRepository(true)
+      .createQueryBuilder('transaction')
+    return queryBuilder
+      .where({ transactionType: ActivityType.Sale, maker: address })
+      .orWhere({ transactionType: ActivityType.Sale, taker: address })
+      .orderBy({ 'transaction.updatedAt': 'DESC' })
+      .leftJoinAndSelect('transaction.activity', 'activity')
+      .cache(true)
+      .getMany()
   }
 
 }
