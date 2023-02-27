1dff2e820210afa8adc375a5b63b0071fd972426
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-02-27 15:45:15 -0500

    chore: only auto log 4xx and 5xx responses (#900)

--- packages/gql/src/server.ts
+++ packages/gql/src/server.ts
@@ -202,6 +202,14 @@ export const start = async (): Promise<void> => {
         )
       },
     },
+    customLogLevel: function (_req, res, err) {
+      if (res.statusCode >= 400 && res.statusCode < 500) {
+        return 'warn'
+      } else if (res.statusCode >= 500 || err) {
+        return 'error'
+      }
+      return 'silent'
+    },
   }))
 
   app.use(Sentry.Handlers.requestHandler())
