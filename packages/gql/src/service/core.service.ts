e86cade8f2baedae58f6b4794a2beb1b5f7341d6
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-02-27 09:47:39 -0400

    fix: fix

--- packages/gql/src/service/core.service.ts
+++ packages/gql/src/service/core.service.ts
@@ -47,6 +47,7 @@ export const getWallet = (
   input: gql.WalletInput,
 ): Promise<entity.Wallet> => {
   const { user, repositories } = ctx
+  const { network, chainId, address } = input
   logger.debug('getWallet', { loggedInUserId: user?.id, input })
 
   return repositories.wallet
