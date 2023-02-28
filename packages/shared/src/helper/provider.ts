916618a376e9c9917f980f57c42bbedd994a83f2
Rohan Dhar <deusexmachina.dev@gmail.com>
2023-02-28 09:03:38 -0500

    chore: clean up

--- packages/shared/src/helper/provider.ts
+++ packages/shared/src/helper/provider.ts
@@ -11,15 +11,15 @@ export const provider = (
   if (infura) { // dedicated key
     return new ethers.providers.InfuraProvider(chainId, process.env.INFURA_API_KEY)
   } else if (process.env.USE_ZMOK == 'true' && Number(chainId) == 1) { // zmok only has support for mainnet and rinkeby (feb 2023)
-    logger.info('Using zmok provider [mint-handler]')
+    logger.info('Using zmok provider')
     return new ethers.providers.JsonRpcProvider(`https://api.zmok.io/mainnet/${process.env.ZMOK_API_KEY}`)
   } else if (process.env.USE_INFURA == 'true') {
-    logger.info('Using infura provider [mint-handler]')
+    logger.info('Using infura provider')
     const items = process.env.INFURA_KEY_SET.split(',')
     const randomKey = items[Math.floor(Math.random() * items.length)]
     return new ethers.providers.InfuraProvider(chainId, randomKey)
   } else {
-    logger.info('Using alchemy provider [mint-handler]')
+    logger.info('Using alchemy provider')
     return new ethers.providers.AlchemyProvider(chainId, process.env.ALCHEMY_API_KEY)
   }
 }
