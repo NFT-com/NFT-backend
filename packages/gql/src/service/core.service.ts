67ecacfaf77d29774de579360157a3fd0d137c8f
gmaijoe <gmaijoe@users.noreply.github.com>
2023-03-06 19:49:00 -0400

    Update backend referrals to actually work (#920)
    * fix: update mint profile
    
    * fix: update dev
    
    * fix: more
    
    * fix: update
    
    * fix: debug
    
    * fix: update
    
    * fix: update referrid
    
    * fix: use referralId

--- packages/gql/src/service/core.service.ts
+++ packages/gql/src/service/core.service.ts
@@ -1142,8 +1142,7 @@ export const createProfileFromEvent = async (
       ownerUserId: wallet.userId,
       chainId: chainId || process.env.CHAIN_ID,
     })
-    logger.log('Save incentive action for CREATE_NFT_PROFILE')
-    // save incentive action for CREATE_NFT_PROFILE
+    logger.log(`Save incentive action for CREATE_NFT_PROFILE for userId: ${wallet.userId}, url: ${profileUrl}`)
     const createProfileAction = await repositories.incentiveAction.findOne({
       where: {
         userId: wallet.userId,
@@ -1158,22 +1157,42 @@ export const createProfileFromEvent = async (
         task: defs.ProfileTask.CREATE_NFT_PROFILE,
         point: defs.ProfileTaskPoint.CREATE_NFT_PROFILE,
       })
-      logger.log('Saved incentive action for CREATE_NFT_PROFILE')
+      logger.log(`Saved incentive action for CREATE_NFT_PROFILE for userId: ${wallet.userId}, url ${profileUrl}`)
     }
-    user = await repositories.user.findOne({
-      where: {
-        // defaults
-        username: 'ethereum-' + ethers.utils.getAddress(owner),
-      },
-    })
-    logger.log('Save incentive action for REFER_NETWORK')
-    //save incentive action for REFER_NETWORK
+
+    user = await repositories.user.findById(wallet.userId)
+
+    logger.log(`Save incentive action for REFER_NETWORK for userId: ${user.id}, url: ${profileUrl}`)
     if (user && user.referredBy) {
       const referredInfo = user.referredBy.split('::')
+
       if (referredInfo && referredInfo.length === 2) {
-        const userMadeReferral = await repositories.user.findById(referredInfo[0])
+        const userMadeReferral = await repositories.user.findOne({ where: { referralId: referredInfo[0] } })
         const referredProfileUrl = referredInfo[1]
-        if (userMadeReferral) {
+        const referralKey =`${referredInfo[0]}::${referredProfileUrl}`
+
+        const referredUsers = await repositories.user.find({
+          where: {
+            referredBy: referralKey,
+          },
+        })
+
+        logger.info(`ensuring >= 5 REFER_NETWORK for ${referralKey}, userMadeReferral id: ${userMadeReferral.id} referredUsers length=${referredUsers.length}`)
+        let accepted = 0
+        await Promise.allSettled(
+          referredUsers.map(async (referUser) => {
+            if (referUser.isEmailConfirmed) {
+              const profile = await repositories.profile.findOne({
+                where: {
+                  ownerUserId: referUser.id,
+                },
+              })
+              if (profile) accepted += 1
+            }
+          }),
+        )
+
+        if (accepted >= 5) {
           const referNetworkAction = await repositories.incentiveAction.findOne({
             where: {
               userId: userMadeReferral.id,
