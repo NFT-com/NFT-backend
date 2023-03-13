2ebf7886e671d2305d509822ed5f46be1966b806
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-13 09:44:23 -0400

    fix: bump

--- packages/gql/src/service/nft.service.ts
+++ packages/gql/src/service/nft.service.ts
@@ -747,7 +747,7 @@ export const getNftName = (
   metadataProvider: MetadataProvider = MetadataProvider.All, // by default gets all
 ): string => {
   const tokenName = tokenId
-    ? [`${contractMetadata?.contractMetadata?.name || contractMetadata?.contractMetadata?.openSea?.collectionName}`, BigNumber.from(tokenId).toString()].join(' ')
+    ? [`${contractMetadata?.contractMetadata?.name || contractMetadata?.contractMetadata?.openSea?.collectionName || alchemyMetadata?.contract?.address || ''}`, `#${BigNumber.from(tokenId).toString()}`].join(' ')
     : ''
 
   if (metadataProvider === MetadataProvider.Alchemy) {
