534d93fbf0c934491f345bf2bc1edce7950846af
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-03-17 13:25:31 -0400

    feat: NFT-1766 add like model (#974)
    * feat: NFT-1766 add like model
    
    * chore: export like repo and add to repo object

---
+++ packages/shared/src/db/repository/like.repository.ts
@@ -0,0 +1,10 @@
+import { Like } from '../entity'
+import { BaseRepository } from './base.repository'
+
+export class LikeRepository extends BaseRepository<Like> {
+
+  constructor() {
+    super(Like)
+  }
+
+}
