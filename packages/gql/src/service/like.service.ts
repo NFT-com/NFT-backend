c0a730903d13a36a9f8c9236eafd0384bdaa9091
Tim Morgan <2998891+t-morgan@users.noreply.github.com>
2023-03-20 11:20:23 -0400

    Feat/nft 1768 unset like (#977)
    * chore: refactor like and profile service to use singleton namespace
    
    * feat: unset like

--- packages/gql/src/service/like.service.ts
+++ packages/gql/src/service/like.service.ts
@@ -1,23 +1,38 @@
-import { _logger, db, entity } from '@nftcom/shared'
-
-export type SetLikeArgs = { likedById: string; likedId: string; likedType: entity.LikeableType }
+import { appError } from '@nftcom/error-types'
+import { db, entity } from '@nftcom/shared'
 
 export function getLikeService(repos: db.Repository = db.newRepositories()): { [key: string]: any } {
+  type SetLikeArgs = { likedById: string; likedId: string; likedType: entity.LikeableType }
   async function setLike({ likedById, likedId, likedType }: SetLikeArgs): Promise<entity.Like> {
     const setLikeArgs = { likedById, likedId, likedType }
     if (!likedById || !likedId || !likedType) {
       throw new Error(`Missing property or property undefined in ${setLikeArgs}`)
     }
     if (!Object.values(entity.LikeableType).includes(likedType)) {
-      throw new Error(`${likedType} cannot be liked`)
+      throw appError.buildInvalid(`${likedType} cannot be liked`, 'LIKE_INVALID')
     }
     if (await repos.like.find({ where: setLikeArgs })) {
-      throw new Error(`${likedType} already liked`)
+      throw appError.buildExists(`${likedType} already liked`, 'LIKE_ALREADY_EXISTS')
     }
     return repos.like.save(setLikeArgs)
   }
 
+  async function unsetLike(id: string, likedById: string): Promise<boolean> {
+    if (!likedById) {
+      throw appError.buildInvalid('Wallet has no preferred profile', 'UNSET_LIKE_INVALID')
+    }
+    const like = await repos.like.findById(id)
+    if (!like) {
+      throw appError.buildNotFound('Like not found', 'UNSET_LIKE_NOT_FOUND')
+    } else if (like.likedById !== likedById) {
+      throw appError.buildForbidden('Wallet cannot unset like', 'UNSET_LIKE_FORBIDDEN')
+    }
+    return repos.like.hardDeleteByIds([id])
+  }
+
   return {
     setLike,
+    unsetLike,
   }
 }
+export const likeService = getLikeService()
