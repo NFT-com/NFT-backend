f5dba2bc671aab8a04f797097d2d91d75763ea8c
Ryo Lambert <ryo@immutableholdings.com>
2023-03-20 15:57:00 -0600

    fix: remove logger from collection repo

--- packages/shared/src/db/repository/collection.repository.ts
+++ packages/shared/src/db/repository/collection.repository.ts
@@ -1,26 +1,16 @@
 import { FindOneOptions, FindOptionsSelect } from 'typeorm'
 
-import { _logger } from '@nftcom/shared'
 import { db } from '@nftcom/shared/db'
 import { Collection } from '@nftcom/shared/db/entity'
 
 import { BaseRepository } from './base.repository'
 
-export type Logger = ReturnType<typeof _logger.Factory>
-
 export class CollectionRepository extends BaseRepository<Collection> {
 
-  constructor(public logger: Logger) {
+  constructor() {
     super(Collection)
   }
 
-  /**
-   * Finds a collection by its contract address.
-   * @param {string} address - the contract address
-   * @param {string} chainId - the chain id of the collection
-   * @param {true} [isOfficial=true] - whether the collection is official
-   * @returns {Promise<Collection>} - the collection
-   */
   public findByContractAddress = (address: string, chainId: string, isOfficial?: true): Promise<Collection> => {
     const whereQuery = { contract: address, deletedAt: null, chainId }
 
@@ -47,11 +37,6 @@ export class CollectionRepository extends BaseRepository<Collection> {
       ${cursor ? cursorAndLimit : limit ? limitOnly : ''}`, [cursor, limit].filter(x => !!x))
   }
 
-  /**
-   * Find all official collections.
-   * @param {Record<keyof Collection, boolean>} [select] - The fields to select.
-   * @returns A promise that resolves to an array of collections.
-   */
   findAllOfficial<T extends FindOptionsSelect<Collection>>(select?: T): T extends Pick<FindOneOptions<Collection>, 'select'> ? Promise<Pick<FindOneOptions<Collection>, 'select'>[]> : Promise<Collection[]> {
     return this.getRepository(true).find({
       where: {
