2fe980f5d8d2db473dd9cb805463387911870d7f
t-morgan <tim@immutableholdings.com>
2023-03-21 14:53:34 -0400

    fix: change unsetLike params to match setLike

--- packages/gql/src/test/service/like.service.unit.spec.ts
+++ packages/gql/src/test/service/like.service.unit.spec.ts
@@ -81,22 +81,24 @@ describe('like service', () => {
 
   describe('getLikeCount', () => {
     it('gets a count of likes', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await Promise.all([
-        likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }),
-        likeService.setLike({ likedById: '2', likedId: '2', likedType: LikeableType.NFT }),
-        likeService.setLike({ likedById: '3', likedId: '2', likedType: LikeableType.NFT }),
+        likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
+        likeService.setLike({ likedById: '2', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
+        likeService.setLike({ likedById: '3', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
       ])
       const result = await likeService.getLikeCount('2')
       expect(result).toBe(3)
     })
 
     it('gets a zero count of likes when there are none', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await Promise.all([
-        likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }),
-        likeService.setLike({ likedById: '2', likedId: '2', likedType: LikeableType.NFT }),
-        likeService.setLike({ likedById: '3', likedId: '2', likedType: LikeableType.NFT }),
+        likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
+        likeService.setLike({ likedById: '2', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
+        likeService.setLike({ likedById: '3', likedId: '2', likedType: LikeableType.NFT }, 'userId'),
       ])
       const result = await likeService.getLikeCount('1')
       expect(result).toBe(0)
@@ -110,15 +112,17 @@ describe('like service', () => {
 
   describe('isLikedByUser', () => {
     it('should be true if the user likes the entity', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
-      await likeService.setLike({ likedById: 'trueProfile', likedId: '2', likedType: LikeableType.NFT })
+      await likeService.setLike({ likedById: 'trueProfile', likedId: '2', likedType: LikeableType.NFT }, 'userId')
       const result = await likeService.isLikedByUser('2', 'true')
       expect(result).toBe(true)
     })
 
     it('should be false if the user does not like the entity', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
-      await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Collection })
+      await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Collection }, 'userId')
       const result = await likeService.isLikedByUser('2', 'testUser')
       expect(result).toBe(false)
     })
@@ -132,8 +136,9 @@ describe('like service', () => {
   
   describe('setLike', () => {
     it('sets a like for an NFT', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
-      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT })
+      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }, 'userId')
 
       expect(result).toEqual({
         createdAt: new Date('2015-07-30T15:26:13.000Z'),
@@ -146,22 +151,24 @@ describe('like service', () => {
     })
 
     it('does not set like for an NFT that is already set', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await likeService.setLike({
         likedById: 'nftLikeSetAlreadyLikedBy',
         likedId: 'nftLikeSetAlreadyLiked',
         likedType: LikeableType.NFT,
-      })
+      }, 'userId')
       await expect(likeService.setLike({
         likedById: 'nftLikeSetAlreadyLikedBy',
         likedId: 'nftLikeSetAlreadyLiked',
         likedType: LikeableType.NFT,
-      })).rejects.toThrow('NFT already liked')
+      }, 'userId')).rejects.toThrow('NFT already liked')
     })
 
     it('sets a like for a Collection', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
-      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Collection })
+      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Collection }, 'userId')
 
       expect(result).toEqual({
         createdAt: new Date('2015-07-30T15:26:13.000Z'),
@@ -174,22 +181,23 @@ describe('like service', () => {
     })
 
     it('does not set like for a Collection that is already set', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await likeService.setLike({
         likedById: 'collectionLikeSetAlreadyLikedBy',
         likedId: 'collectionLikeSetAlreadyLiked',
         likedType: LikeableType.Collection,
-      })
+      }, 'userId')
       await expect(likeService.setLike({
         likedById: 'collectionLikeSetAlreadyLikedBy',
         likedId: 'collectionLikeSetAlreadyLiked',
         likedType: LikeableType.Collection,
-      })).rejects.toThrow('Collection already liked')
+      }, 'userId')).rejects.toThrow('Collection already liked')
     })
 
     it('sets a like for a Profile', async () => {
       const likeService = getLikeService(repos)
-      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Profile })
+      const result = await likeService.setLike({ likedById: '1', likedId: '2', likedType: LikeableType.Profile }, 'userId')
 
       expect(result).toEqual({
         createdAt: new Date('2015-07-30T15:26:13.000Z'),
@@ -202,26 +210,28 @@ describe('like service', () => {
     })
 
     it('does not set like for a Profile that is already set', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await likeService.setLike({
         likedById: 'profileLikeSetAlreadyLikedBy',
         likedId: 'profileLikeSetAlreadyLiked',
         likedType: LikeableType.Profile,
-      })
+      }, 'userId')
       await expect(likeService.setLike({
         likedById: 'profileLikeSetAlreadyLikedBy',
         likedId: 'profileLikeSetAlreadyLiked',
         likedType: LikeableType.Profile,
-      })).rejects.toThrow('Profile already liked')
+      }, 'userId')).rejects.toThrow('Profile already liked')
     })
 
     it('does not set like for invalid type', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(true)
       const likeService = getLikeService(repos)
       await expect(likeService.setLike({
         likedById: '1',
         likedId: '2',
         likedType: 'Walnut' as LikeableType,
-      })).rejects.toThrow('Walnut cannot be liked')
+      }, 'userId')).rejects.toThrow('Walnut cannot be liked')
     })
 
     it('requires likedById', async () => {
@@ -230,7 +240,7 @@ describe('like service', () => {
         likedById: undefined,
         likedId: '2',
         likedType: LikeableType.NFT,
-      })).rejects.toThrow(/^Missing property or property undefined in .*$/)
+      }, 'userId')).rejects.toThrow(/^Missing property or property undefined in .*$/)
     })
 
     it('requires likedId', async () => {
@@ -239,16 +249,26 @@ describe('like service', () => {
         likedById: '1',
         likedId: undefined,
         likedType: LikeableType.NFT,
-      })).rejects.toThrow(/^Missing property or property undefined in .*$/)
+      }, 'userId')).rejects.toThrow(/^Missing property or property undefined in .*$/)
     })
 
-    it('required likedType', async () => {
+    it('requires likedType', async () => {
       const likeService = getLikeService(repos)
       await expect(likeService.setLike({
         likedById: '1',
         likedId: '2',
         likedType: undefined,
-      })).rejects.toThrow(/^Missing property or property undefined in .*$/)
+      }, 'userId')).rejects.toThrow(/^Missing property or property undefined in .*$/)
+    })
+
+    it('requires user to own profile likedBy', async () => {
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(false)
+      const likeService = getLikeService(repos)
+      await expect(likeService.setLike({
+        likedById: '1',
+        likedId: '2',
+        likedType: LikeableType.Profile,
+      }, 'userId')).rejects.toThrow('User cannot set like')
     })
   })
 
@@ -260,31 +280,43 @@ describe('like service', () => {
         likedById: 'likedById',
         likedId: '1',
         likedType: LikeableType.NFT,
-      })
-      const result = await likeService.unsetLike(like.id, 'likedById')
+      }, 'userId')
+      const result = await likeService.unsetLike({
+        likedById: like.likedById,
+        likedId: like.likedId,
+        likedType: like.likedType,
+      }, 'userId')
       expect(result).toBe(true)
     })
     it('should throw invalid when no user id', async () => {
       const likeService = getLikeService(repos)
-      await expect(likeService.unsetLike('1', undefined))
+      await expect(likeService.unsetLike({ likedById: '1', likedId: '2', likedType: LikeableType.NFT }, undefined))
         .rejects.toThrow('unsetLike requires likedByUserId')
     })
 
     it('should throw not found when no like found', async () => {
       const likeService = getLikeService(repos)
-      await expect(likeService.unsetLike('-1', 'testLikedById'))
+      await expect(likeService.unsetLike({
+        likedById: '0',
+        likedId: '-1',
+        likedType: LikeableType.NFT,
+      }, 'testLikedById'))
         .rejects.toThrow('Like not found')
     })
 
     it('should throw forbidden when likedByUserId does not own the profile likedBY', async () => {
-      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValue(false)
+      jest.spyOn(profileService, 'isProfileOwnedByUser').mockResolvedValueOnce(true).mockResolvedValue(false)
       const likeService = getLikeService(repos)
       const like = await likeService.setLike({
         likedById: 'differentId',
         likedId: '1',
         likedType: LikeableType.NFT,
-      })
-      await expect(likeService.unsetLike(like.id, 'testLikedById'))
+      }, 'userId')
+      await expect(likeService.unsetLike({
+        likedById: like.likedById,
+        likedId: like.likedId,
+        likedType: like.likedType,
+      }, 'testLikedById'))
         .rejects.toThrow('User cannot unset like')
     })
   })
