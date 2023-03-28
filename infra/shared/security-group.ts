1728a10d7180ddde9ef7660e3b419d2d5571f49d
Anthony Rozario <anthony@immutableholdings.com>
2023-03-27 18:42:14 -0700

    fix: adding dmsSG to output

--- infra/shared/security-group.ts
+++ infra/shared/security-group.ts
@@ -11,6 +11,7 @@ export type SGOutput = {
   typesense: awsEC2.SecurityGroup
   web: awsEC2.SecurityGroup
   webEcs: awsEC2.SecurityGroup
+  dmsSG: awsEC2.SecurityGroup
 }
 
 const buildIngressRule = (
@@ -114,7 +115,7 @@ export const createSecurityGroups = (config: pulumi.Config, vpc: ec2.Vpc): SGOut
           buildIngressRule(5432, 'tcp', [web.id]),
           buildIngressRule(5432, 'tcp', [webEcs.id]),
           buildIngressRule(5432, 'tcp', [internalEcs.id]),
-          buildIngressRule(5432, 'tcp', [dmsSG.id])
+          buildIngressRule(5432, 'tcp', [dmsSG.id]),
           buildIngressRule(5432, 'tcp', [pulumi.output('sg-0bad265e467cdec96')]), // Bastion Host
           buildIngressRule(5432, 'tcp', [pulumi.output('sg-0bd5dceea498f0356')]), // Prod Stream ECS Cluster 
         ]
