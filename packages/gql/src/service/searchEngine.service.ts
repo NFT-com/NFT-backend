e38e44c59c9752488b6998c25a10d183f7d83edf
Joey <joey@nft.com>
2023-04-11 09:56:41 -0500

    fix: dev typesense to use mainnet contracts (#1035)
    Co-authored-by: Joey Punzel <joey@immutableholdings.com>

--- packages/gql/src/service/searchEngine.service.ts
+++ packages/gql/src/service/searchEngine.service.ts
@@ -11,14 +11,8 @@ import { getListingCurrencyAddress, getListingPrice, listingMapFrom, TxActivityD
 
 const logger = _logger.Factory('searchEngine.service', _logger.Context.Typesense)
 
-const TYPESENSE_HOST = process.env.TYPESENSE_HOST
-const PROFILE_CONTRACT = TYPESENSE_HOST.startsWith('dev')
-  ? '0x9Ef7A34dcCc32065802B1358129a226B228daB4E'
-  : '0x98ca78e89Dd1aBE48A53dEe5799F24cC1A462F2D'
-
-const GK_CONTRACT = TYPESENSE_HOST.startsWith('dev')
-  ? '0xe0060010c2c81A817f4c52A9263d4Ce5c5B66D55'
-  : '0x8fB5a7894AB461a59ACdfab8918335768e411414'
+const PROFILE_CONTRACT = '0x98ca78e89Dd1aBE48A53dEe5799F24cC1A462F2D'
+const GK_CONTRACT = '0x8fB5a7894AB461a59ACdfab8918335768e411414'
 
 const LARGEST_COLLECTIONS = defs.LARGE_COLLECTIONS.slice(0, 3)
 export const SearchEngineService = (client = SearchEngineClient.create(), repos: any = db.newRepositories()): any => {
