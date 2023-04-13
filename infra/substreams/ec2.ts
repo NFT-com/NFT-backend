1156b1aa76758079de9ec65c5e25d9828c9f5e19
Anthony Rozario <anthony@immutableholdings.com>
2023-04-13 13:22:02 -0700

    creating a single db string

--- infra/substreams/ec2.ts
+++ infra/substreams/ec2.ts
@@ -22,8 +22,11 @@ const db_pass = process.env.DB_PASSWORD;
 
 
 export const createUserData = ( pgCluster : SubstreamRDSOutput) : string => {
-    const db_user = pgCluster.main.masterUsername; 
-    const db_host = pulumi.interpolate `${pgCluster.main.endpoint}`; 
+    let db_user = pgCluster.main.masterUsername; 
+    let db_host = pulumi.interpolate `${pgCluster.main.endpoint}`; 
+
+    let db_string = pulumi.all([db_user, db_host]).apply(([db_user, db_host]) => `psql://${{db_user}}:${db_pass}@${db_host}/app?sslmode=disable`); 
+    
 
     //const dbString = pulumi.concat(pgCluster.main.endpoint)
 
@@ -84,7 +87,7 @@ cd substreams-sync
 
 #Initialize PG DBs 
 echo "Initializing Substreams Databases..."
-substreams-sink-postgres setup "psql://app:${db_pass}@${pgCluster.main.endpoint}/app?sslmode=disable" ./docs/nftLoader/schema.sql
+substreams-sink-postgres setup "${db_string}" ./docs/nftLoader/schema.sql
 
 substreams-sink-postgres setup "psql://app:${db_pass}@${pgCluster.main.endpoint}/app?sslmode=disable" ./example_consumer/notifyConsumer.sql
 
