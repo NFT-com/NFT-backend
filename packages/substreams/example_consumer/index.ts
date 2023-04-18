3abc7a46b49e984e15cfbf08537e76554ab0916d
gmaijoe <gmaijoe@users.noreply.github.com>
2023-04-18 10:57:19 -0400

    Fix lint, format, and replace old infura keys (#1050)
    * fix: ok
    
    * fix: ok
    
    * fix: ok

--- packages/substreams/example_consumer/index.ts
+++ packages/substreams/example_consumer/index.ts
@@ -1,27 +1,40 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Client } from 'pg';
+import { Client } from 'pg'
 
-const connectionString = 'postgres://dev-node:insecure-change-me-in-prod@localhost:5432/dev-node?sslmode=disable';
+const connectionString = 'postgres://dev-node:insecure-change-me-in-prod@localhost:5432/dev-node?sslmode=disable'
 
 const client = new Client({
   connectionString,
-});
+})
 
-client.connect();
+client.connect()
 
-client.query('LISTEN transfers');
+client.query('LISTEN transfers')
 
-console.log('Waiting for new inserts...');
+console.log('Waiting for new inserts...')
 
 client.on('notification', (msg: any) => {
-  const [schema, blockNumber, tokenId, contractAddress, quantity, fromAddress, toAddress, txHash, timestamp] = msg.payload.split("|");
+  const [schema, blockNumber, tokenId, contractAddress, quantity, fromAddress, toAddress, txHash, timestamp] =
+    msg.payload.split('|')
 
   if (fromAddress == '0000000000000000000000000000000000000000') {
-    console.log(`[MINTED]: ${schema}/${contractAddress}/${tokenId} to ${toAddress}, ${quantity > 1 ? `quantity=${quantity}, ` : ''}`);
+    console.log(
+      `[MINTED]: ${schema}/${contractAddress}/${tokenId} to ${toAddress}, ${
+        quantity > 1 ? `quantity=${quantity}, ` : ''
+      }`,
+    )
   } else if (toAddress == '0000000000000000000000000000000000000000') {
-    console.log(`[BURNED]: ${schema}/${contractAddress}/${tokenId} from ${fromAddress}, ${quantity > 1 ? `quantity=${quantity}, ` : ''}`);
+    console.log(
+      `[BURNED]: ${schema}/${contractAddress}/${tokenId} from ${fromAddress}, ${
+        quantity > 1 ? `quantity=${quantity}, ` : ''
+      }`,
+    )
   } else {
-    console.log(`[TRANSFERRED]: ${schema}/${contractAddress}/${tokenId} from ${fromAddress} to ${toAddress}, ${quantity > 1 ? `quantity=${quantity}, ` : ''}`);
+    console.log(
+      `[TRANSFERRED]: ${schema}/${contractAddress}/${tokenId} from ${fromAddress} to ${toAddress}, ${
+        quantity > 1 ? `quantity=${quantity}, ` : ''
+      }`,
+    )
   }
-});
+})
