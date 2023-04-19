ca458c51d34550781c9136423a6640e7a64c988f
Anthony Rozario <59712821+nightmar39@users.noreply.github.com>
2023-04-19 11:56:22 -0700

    fix:removing build_dev workflow for substreams, modified instance and cluster to be in public subnets, updated security group rule for RDS to allow access (#1053)

--- infra/substreams/ec2.ts
+++ infra/substreams/ec2.ts
@@ -133,7 +133,7 @@ export const createEC2Resources = (
   const stage = getStage()
   const getInstanceSubnet = (subnetGroups: vpcSubnets): string => {
     //const index = (Math.random() * (numSubnets - 1)); // pick a random subnet from correct subnet group
-    const subnetGroup = isProduction() ? subnetGroups.privateSubnets : subnetGroups.publicSubnets
+    const subnetGroup = subnetGroups.publicSubnets
     return subnetGroup[0]
   }
 
