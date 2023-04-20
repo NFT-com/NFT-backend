8cdbbaec863269117dddd919a19d1d911176768e
Anthony Rozario <anthony@immutableholdings.com>
2023-04-19 18:40:21 -0700

    added provider resource, fixed github action

--- infra/cross-account/index.ts
+++ infra/cross-account/index.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import * as upath from 'upath'
 import * as pulumi from '@pulumi/pulumi'
+import * as aws from '@pulumi/aws'
+
 
 import { deployInfra } from '../helper'
 import { createQueue } from './sqs'
@@ -8,10 +12,22 @@ const pulumiProgram = async() : Promise<Record<string, any> | void> => {
 
     const queue = createQueue()
 
+    const dev_provider = new aws.Provider("target-account-provider", {
+        assumeRole: {
+            roleArn: "arn:aws:iam::135063527524:role/cicd_cross_account",
+            sessionName: "pulumi-target-account-session",
+        },
+        region: "us-east-1",
+        profileName: "source-account-credentials"
+    });
+    
+
     return {
         queueName: queue.name
     }
 }
+
+
 
 export const createQueues = (preview?: boolean): Promise<pulumi.automation.OutputMap> => {
     const stackName = `${process.env.STAGE}.shared.${process.env.AWS_REGION}`
--- infra/cross-account/sqs.ts
+++ infra/cross-account/sqs.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as aws from '@pulumi/aws'
 
 import { getResourceName, getStage, joinStringsByDash } from '../helper'
 
 export type sqsOutput = {
-    queue: aws.sqs.queue
+    queue: aws.sqs.Queue
 }
 
-export const createQueue = () : aws.sqs.queue => {
+export const createQueue = () : aws.sqs.Queue => {
     return new aws.sqs.Queue("cross-account-queue", {
         name: getResourceName("cross-account-queue"),
         delaySeconds: 90
