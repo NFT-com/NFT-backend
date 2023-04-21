3a7c3da3d5b4d4d335b19c6942a18e9cc6d7da1a
Anthony Rozario <anthony@immutableholdings.com>
2023-04-21 10:48:11 -0700

    adding updated resource file

--- infra/cross-account/sqs.ts
+++ infra/cross-account/sqs.ts
@@ -1,16 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as aws from '@pulumi/aws'
+import * as pulumi from '@pulumi/pulumi'
 
-import { getResourceName, getStage, joinStringsByDash } from '../helper'
+import { getResourceName } from '../helper'
 
 export type sqsOutput = {
     queue: aws.sqs.Queue
 }
 
-export const createQueue = () : aws.sqs.Queue => {
+export const createQueue = (provider : pulumi.Provider ) : aws.sqs.Queue => {
     return new aws.sqs.Queue("cross-account-queue", {
         name: getResourceName("cross-account-queue"),
         delaySeconds: 90
-    })
+    },
+    {
+        provider: provider
+    }
+    )
 }
 
