b8f5fb4e554c61568fb7f5e09e85dded1de0042c
t-morgan <tim@immutableholdings.com>
2023-04-24 11:13:07 -0400

    chore: update cronjobs to use pnpm

--- cronjobs/monitors/hidden-nfts/scripts/package.sh
+++ cronjobs/monitors/hidden-nfts/scripts/package.sh
@@ -6,11 +6,8 @@ BUILD_PATH_LAYERS=$CWD/layers
 cd $CWD
 
 # Package typescript code
-npm install --prefer-offline --platform=linux --arch=x64
-cd $CWD/packages/shared
-npm run build
-cd -
-npx -y nx run monitors-hidden-nfts:build:production
+pnpm install --filter=@nftcom/hidden-nfts...
+pnpm dlx turbo run build --filter=@nftcom/hidden-nfts...
 
 echo "zip build directory"
 cd dist/cronjobs/monitors
@@ -18,24 +15,23 @@ zip -q -r archive.zip hidden-nfts/*
 
 # Package node_modules
 mkdir -p $BUILD_PATH_LAYERS
-cp $CWD/package-lock.json $BUILD_PATH_LAYERS/package-lock.json
-cp $CWD/package.json $BUILD_PATH_LAYERS/package.json
+cp $CWD/pnpm-lock.json $BUILD_PATH_LAYERS/pnpm-lock.json
+cp $CWD/pnpm-workspace.json $BUILD_PATH_LAYERS/pnpm-workspace.json
+mkdir -p $BUILD_PATH_LAYERS/cronjobs/monitors/hidden-nfts
+cp $CWD/cronjobs/monitors/hidden-nfts/package.json $BUILD_PATH_LAYERS/cronjobs/monitors/hidden-nfts/package.json
 mkdir -p $BUILD_PATH_LAYERS/packages/shared
-cp $CWD/packages/shared/package.json $BUILD_PATH_LAYERS/packages/shared/package.json
 
 cd $BUILD_PATH_LAYERS
 echo "installing production only dependencies"
-mv $CWD/packages/shared/dist packages/shared/dist
-npm ci --production --omit=dev --platform=linux --arch=x64 --ignore-scripts
+mv $CWD/dist/packages/shared packages/shared
+pnpm install --filter=@nftcom/hidden-nfts... --prod
 
 echo "zip node_modules directory"
 mkdir -p ./nodejs
-mv node_modules nodejs/node_modules
-rm ./nodejs/node_modules/@nftcom/shared
-mv ./packages/shared nodejs/node_modules/@nftcom/shared
+mv ./packages nodejs
 rm -rf packages
-rm package.json
-rm package-lock.json
+rm pnpm-lock.json
+rm pnpm-workspace.json
 
 zip -q -r archive.zip *
 rm -rf nodejs
