7c42d900e9e1ea0123539d4935060a9dec53d659
t-morgan <tim@immutableholdings.com>
2023-04-24 11:28:33 -0400

    chore: save progress hidden-nft lambda build

--- cronjobs/monitors/hidden-nfts/scripts/build_lambda.sh
+++ cronjobs/monitors/hidden-nfts/scripts/build_lambda.sh
@@ -7,7 +7,7 @@ cd $CWD
 
 # Package typescript code
 pnpm install --filter=@nftcom/hidden-nfts...
-pnpm dlx turbo run build --filter=@nftcom/hidden-nfts...
+pnpm dlx turbo run build --filter=@nftcom/hidden-nfts... --force
 
 echo "zip build directory"
 cd dist/cronjobs/monitors
@@ -15,23 +15,23 @@ zip -q -r archive.zip hidden-nfts/*
 
 # Package node_modules
 mkdir -p $BUILD_PATH_LAYERS
-cp $CWD/pnpm-lock.json $BUILD_PATH_LAYERS/pnpm-lock.json
-cp $CWD/pnpm-workspace.json $BUILD_PATH_LAYERS/pnpm-workspace.json
+cp $CWD/pnpm-lock.yaml $BUILD_PATH_LAYERS/pnpm-lock.yaml
+cp $CWD/pnpm-workspace.yaml $BUILD_PATH_LAYERS/pnpm-workspace.yaml
 mkdir -p $BUILD_PATH_LAYERS/cronjobs/monitors/hidden-nfts
 cp $CWD/cronjobs/monitors/hidden-nfts/package.json $BUILD_PATH_LAYERS/cronjobs/monitors/hidden-nfts/package.json
 mkdir -p $BUILD_PATH_LAYERS/packages/shared
 
 cd $BUILD_PATH_LAYERS
 echo "installing production only dependencies"
-mv $CWD/dist/packages/shared packages/shared
+mv $CWD/dist/packages/shared packages
 pnpm install --filter=@nftcom/hidden-nfts... --prod
 
 echo "zip node_modules directory"
 mkdir -p ./nodejs
 mv ./packages nodejs
 rm -rf packages
-rm pnpm-lock.json
-rm pnpm-workspace.json
+rm pnpm-lock.yaml
+rm pnpm-workspace.yaml
 
 zip -q -r archive.zip *
 rm -rf nodejs
