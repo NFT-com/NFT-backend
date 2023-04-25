b0d5d0160d7995d22afe355759c107ad99fcc97a
Anthony Rozario <anthony@immutableholdings.com>
2023-04-25 11:42:41 -0700

    fixing index to deploy to second account

--- infra/cross-account-queues/index.ts
+++ infra/cross-account-queues/index.ts
@@ -18,7 +18,7 @@ const pulumiProgram = async() : Promise<Record<string, any> | void> => {
         queueName: queue.name
     }
 }
-
+//commenting for deploy
 export const createQueues = (preview?: boolean): Promise<pulumi.automation.OutputMap> => {
     const stackName = `${process.env.STAGE}.${process.env.ACCOUNT}.queues.${process.env.AWS_REGION}`
     const workDir = upath.joinSafe(__dirname, 'stack')
